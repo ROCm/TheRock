f3e591351fc6ffa71d04047fb7251919fad65a50
Marius Brehler <marius.brehler@amd.com>
2025-03-24 12:16:13 +0000

    Enable `-DTHEROCK_BUILD_TESTING` option for hipBLAS

--- math-libs/BLAS/post_hook_hipBLAS.cmake
+++ math-libs/BLAS/post_hook_hipBLAS.cmake
@@ -8,15 +8,17 @@ therock_set_install_rpath(
     .
 )
 
-therock_set_install_rpath(
-  TARGETS
-    hipblas-bench
-    hipblas-test
-    hipblas_v2-bench
-    hipblas_v2-test
-  PATHS
-    ../lib
-    ../lib/host-math/lib
-    ../lib/llvm/lib
-    ../lib/rocm_sysdeps/lib
-)
+if(THEROCK_BUILD_TESTING)
+  therock_set_install_rpath(
+    TARGETS
+      hipblas-bench
+      hipblas-test
+      hipblas_v2-bench
+      hipblas_v2-test
+    PATHS
+      ../lib
+      ../lib/host-math/lib
+      ../lib/llvm/lib
+      ../lib/rocm_sysdeps/lib
+  )
+endif()
