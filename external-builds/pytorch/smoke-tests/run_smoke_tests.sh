e8eebd053c1631d95d5944fe613156b3b330943d
erman-gurses <nobody@amd.com>
2025-03-25 12:54:24 -0500

    Add script for running smoke tests
    Signed-off-by: erman-gurses <nobody@amd.com>

---
+++ external-builds/pytorch/smoke-tests/run_smoke_tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+echo 'Running inside the container'
+
+echo 'Set path to .local/bin'
+export PATH="$HOME/.local/bin:$PATH"
+
+echo 'Check Python version'
+python3 --version
+
+echo 'List the pip packages'
+pip list
+
+echo 'Install pytest'
+PIP_BREAK_SYSTEM_PACKAGES=1 pip install -U pytest
+
+echo 'Run smoke tests'
+pytest -v external-builds/pytorch/smoke-tests/pytorch_smoke_tests.py
+
+echo 'Task completed!'
