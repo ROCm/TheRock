e197bbd847c10bed57844a9435e89c7768930419
Scott <scott.todd0@gmail.com>
2025-03-25 13:38:35 -0700

    Organize configure_ci.py into sections.

--- build_tools/configure_ci.py
+++ build_tools/configure_ci.py
@@ -38,53 +38,9 @@ import subprocess
 import sys
 from typing import Iterable, List, Mapping, Optional
 
-
-# Paths matching any of these patterns are considered to have no influence over
-# build or test workflows so any related jobs can be skipped if all paths
-# modified by a commit/PR match a pattern in this list.
-SKIPPABLE_PATH_PATTERNS = [
-    "docs/*",
-    "*.gitignore",
-    "*.md",
-    "*LICENSE",
-]
-
-
-def is_path_skippable(path: str) -> bool:
-    """Determines if a given relative path to a file matches any skippable patterns."""
-    return any(fnmatch.fnmatch(path, pattern) for pattern in SKIPPABLE_PATH_PATTERNS)
-
-
-def check_for_non_skippable_path(paths: Optional[Iterable[str]]) -> bool:
-    """Returns true if at least one path is not in the skippable set."""
-    if paths is None:
-        return False
-    return any(not is_path_skippable(p) for p in paths)
-
-
-def get_modified_paths(base_ref: str) -> Optional[Iterable[str]]:
-    """Returns the paths of modified files relative to the base reference."""
-    try:
-        return subprocess.run(
-            ["git", "diff", "--name-only", base_ref],
-            stdout=subprocess.PIPE,
-            check=True,
-            text=True,
-            timeout=60,
-        ).stdout.splitlines()
-    except TimeoutError:
-        print(
-            "Computing modified files timed out. Not using PR diff to determine"
-            " jobs to run.",
-            file=sys.stderr,
-        )
-        return None
-
-
-def get_pr_labels() -> List[str]:
-    """Gets a list of labels applied to a pull request."""
-    labels = json.loads(os.environ.get("PR_LABELS", "[]"))
-    return labels
+# --------------------------------------------------------------------------- #
+# General utilities
+# --------------------------------------------------------------------------- #
 
 
 def set_github_output(d: Mapping[str, str]):
@@ -112,6 +68,69 @@ def write_job_summary(summary: str):
     with open(step_summary_file, "a") as f:
         # Use double newlines to split sections in markdown.
         f.write(summary + "\n\n")
+
+
+# --------------------------------------------------------------------------- #
+# Filtering by modified paths
+# --------------------------------------------------------------------------- #
+
+
+def get_modified_paths(base_ref: str) -> Optional[Iterable[str]]:
+    """Returns the paths of modified files relative to the base reference."""
+    try:
+        return subprocess.run(
+            ["git", "diff", "--name-only", base_ref],
+            stdout=subprocess.PIPE,
+            check=True,
+            text=True,
+            timeout=60,
+        ).stdout.splitlines()
+    except TimeoutError:
+        print(
+            "Computing modified files timed out. Not using PR diff to determine"
+            " jobs to run.",
+            file=sys.stderr,
+        )
+        return None
+
+
+# Paths matching any of these patterns are considered to have no influence over
+# build or test workflows so any related jobs can be skipped if all paths
+# modified by a commit/PR match a pattern in this list.
+SKIPPABLE_PATH_PATTERNS = [
+    "docs/*",
+    "*.gitignore",
+    "*.md",
+    "*LICENSE",
+]
+
+
+def is_path_skippable(path: str) -> bool:
+    """Determines if a given relative path to a file matches any skippable patterns."""
+    return any(fnmatch.fnmatch(path, pattern) for pattern in SKIPPABLE_PATH_PATTERNS)
+
+
+def check_for_non_skippable_path(paths: Optional[Iterable[str]]) -> bool:
+    """Returns true if at least one path is not in the skippable set."""
+    if paths is None:
+        return False
+    return any(not is_path_skippable(p) for p in paths)
+
+
+# --------------------------------------------------------------------------- #
+# Working with pull request labels
+# --------------------------------------------------------------------------- #
+
+
+def get_pr_labels() -> List[str]:
+    """Gets a list of labels applied to a pull request."""
+    labels = json.loads(os.environ.get("PR_LABELS", "[]"))
+    return labels
+
+
+# --------------------------------------------------------------------------- #
+# Core script logic
+# --------------------------------------------------------------------------- #
 
 
 def main():
