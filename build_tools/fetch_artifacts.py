773c6398e916b81167212b6ef55e87aca504f340
geomin12 <geomin12@amd.com>
2025-03-26 09:59:33 -0700

    let us try with these

--- build_tools/fetch_artifacts.py
+++ build_tools/fetch_artifacts.py
@@ -32,10 +32,10 @@ def retrieve_base_artifacts(run_id, build_dir):
         "core-runtime_lib",
         "sysdeps_lib",
         "base_lib",
-        "amd-llvm_lib",
         "amd-llvm_run",
-        # "core-hip_lib",
-        # "host-blas_lib",
+        "amd-llvm_lib",
+        "core-hip_lib",
+        "host-blas_lib",
         # "rocprofiler-sdk_lib",
         # "host-suite-sparse_lib"
     ]
