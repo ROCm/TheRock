262aa2384e4839eb32286851a473c3a9a70b2dc3
geomin12 <geomin12@amd.com>
2025-03-25 19:22:19 -0700

    linting

--- build_tools/fetch_artifacts.py
+++ build_tools/fetch_artifacts.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# This script provides a somewhat dynamic way to 
+# This script provides a somewhat dynamic way to
 # retrieve artifacts from s3
 
 import argparse
@@ -8,9 +8,11 @@ import sys
 
 GENERIC_VARIANT = "generic"
 
+
 def log(*args, **kwargs):
     print(*args, **kwargs)
     sys.stdout.flush()
+
 
 def s3_exec(variant, package, run_id, build_dir):
     cmd = [
@@ -19,11 +21,12 @@ def s3_exec(variant, package, run_id, build_dir):
         "cp",
         f"s3://therock-artifacts/{run_id}/{package}_{variant}.tar.xz",
         build_dir,
-        "--no-sign-request"
+        "--no-sign-request",
     ]
     log(f"++ Exec [{cmd}]")
-    # subprocess.run(cmd)
-    
+    subprocess.run(cmd)
+
+
 def retrieve_base_artifacts(run_id, build_dir):
     base_artifacts = [
         "core-runtime_run",
@@ -34,10 +37,11 @@ def retrieve_base_artifacts(run_id, build_dir):
         "core-hip_lib",
         "host-blas_lib",
         "rocprofiler-sdk_lib",
-        "host-suite-sparse_lib"
+        "host-suite-sparse_lib",
     ]
     for base_artifact in base_artifacts:
         s3_exec(GENERIC_VARIANT, base_artifact, run_id, build_dir)
+
 
 def retrieve_enabled_artifacts(args, test_enabled):
     base_artifact_path = []
@@ -62,6 +66,7 @@ def retrieve_enabled_artifacts(args, test_enabled):
 
     return enabled_artifacts
 
+
 def run(args):
     run_id = args.run_id
     target = args.target
@@ -71,85 +76,73 @@ def run(args):
     enabled_artifacts = retrieve_enabled_artifacts(args, test_enabled)
 
     for enabled_artifact in enabled_artifacts:
-        s3_exec(
-            f"{target}",
-            enabled_artifact,
-            run_id,
-            build_dir
-        )
-    
+        s3_exec(f"{target}", enabled_artifact, run_id, build_dir)
+
 
 def main(argv):
     parser = argparse.ArgumentParser(prog="fetch_artifacts")
     parser.add_argument(
-        "--run-id",
-        type=str,
-        help="GitHub run ID to retrieve artifacts from"
+        "--run-id", type=str, help="GitHub run ID to retrieve artifacts from"
     )
-    
+
     parser.add_argument(
-        "--target",
-        type=str,
-        help="Target variant for specific GPU target"
+        "--target", type=str, help="Target variant for specific GPU target"
     )
-    
+
     parser.add_argument(
         "--build-dir",
         type=str,
         default="build/artifacts",
-        help="Path to the artifact build directory"
-    )
-    
-    parser.add_argument(
-        "--test",
-        help="If flagged, test artifacts will be retrieved",
-        action="store_true"
-    )
-    
-    parser.add_argument(
-        "--blas",
-        help="If flagged, blas artifacts will be retrieved",
-        action="store_true"
+        help="Path to the artifact build directory",
     )
 
     parser.add_argument(
-        "--fft",
-        help="If flagged, fft artifacts will be retrieved",
-        action="store_true"
+        "--test",
+        help="If flagged, test artifacts will be retrieved",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "--blas",
+        help="If flagged, blas artifacts will be retrieved",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "--fft", help="If flagged, fft artifacts will be retrieved", action="store_true"
     )
 
     parser.add_argument(
         "--miopen",
         help="If flagged, miopen artifacts will be retrieved",
-        action="store_true"
+        action="store_true",
     )
 
     parser.add_argument(
         "--prim",
         help="If flagged, prim artifacts will be retrieved",
-        action="store_true"
+        action="store_true",
     )
 
     parser.add_argument(
         "--rand",
         help="If flagged, rand artifacts will be retrieved",
-        action="store_true"
+        action="store_true",
     )
 
     parser.add_argument(
         "--rccl",
         help="If flagged, rccl artifacts will be retrieved",
-        action="store_true"
+        action="store_true",
     )
-    
+
     parser.add_argument(
-        "--all",
-        help="If flagged, all artifacts will be retrieved",
-        action="store_true"
+        "--all", help="If flagged, all artifacts will be retrieved", action="store_true"
     )
 
     args = parser.parse_args(argv)
     run(args)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
