76184b162ad9ba6672b9dfc9f9bdccdee07b5516
geomin12 <geomin12@amd.com>
2025-03-27 16:57:19 -0700

    whoops fixing tests

--- build_tools/configure_ci_test.py
+++ build_tools/configure_ci_test.py
@@ -33,8 +33,8 @@ class ConfigureCITest(TestCase):
     @mock.patch.dict(
         os.environ,
         {
-            "INPUT_LINUX_AMDGPU_FAMILIES": "   gfx942X ,|.\\,  gfx1201X, --   gfx90X",
-            "INPUT_WINDOWS_AMDGPU_FAMILIES": "gfx942X \\., gfx1201X  gfx90X",
+            "INPUT_LINUX_AMDGPU_FAMILIES": "   gfx94X ,|.\\,  gfx1201X, --   gfx90X",
+            "INPUT_WINDOWS_AMDGPU_FAMILIES": "gfx94X \\., gfx1201X  gfx90X",
         },
     )
     def test_valid_workflow_dispatch_matrix_generator(self):
@@ -42,7 +42,7 @@ class ConfigureCITest(TestCase):
             False, True, False
         )
         linux_target_to_compare = [
-            {"runs-on": "linux-mi300-1gpu-ossci-rocm", "target": "gfx942X-dcgpu"}
+            {"runs-on": "linux-mi300-1gpu-ossci-rocm", "target": "gfx94X-dcgpu"}
         ]
         self.assertEqual(linux_target_output, linux_target_to_compare)
         self.assertEqual(windows_target_output, [])
@@ -57,7 +57,7 @@ class ConfigureCITest(TestCase):
     @mock.patch.dict(
         os.environ,
         {
-            "PR_LABELS": '["gfx942X-linux", "gfx1201X-windows", "gfx942X-windows", "gfx1201-linux"]'
+            "PR_LABELS": '["gfx94X-linux", "gfx1201X-windows", "gfx94X-windows", "gfx1201-linux"]'
         },
     )
     def test_valid_pull_request_matrix_generator(self):
@@ -65,7 +65,7 @@ class ConfigureCITest(TestCase):
             True, False, False
         )
         linux_target_to_compare = [
-            {"runs-on": "linux-mi300-1gpu-ossci-rocm", "target": "gfx942X-dcgpu"}
+            {"runs-on": "linux-mi300-1gpu-ossci-rocm", "target": "gfx94X-dcgpu"}
         ]
         self.assertEqual(linux_target_output, linux_target_to_compare)
         self.assertEqual(windows_target_output, [])
