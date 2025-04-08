6f8c5f7e5a4a90f4efc2578b4bb1f81ede6ae5e3
geomin12 <geomin12@amd.com>
2025-04-08 07:37:47 -0700

    PR comments

--- build_tools/configure_release_matrix.py
+++ build_tools/configure_release_matrix.py
@@ -1,34 +1,28 @@
 import os
 import json
 from configure_ci import set_github_output, amdgpu_family_info_matrix
-from datetime import datetime
 
 # This file helps generate a release test matrix for test_release_packages.yml
-
-TODAY_DATE = datetime.today().strftime("%Y%m%d")
 
 
 def main(args):
     assets = json.loads(args.get("asset_files")).get("assets", [])
+    today_date = args.get("today_date")
     release_matrix = []
     for asset in assets:
         asset_name = asset.get("name", "")
         # Test only today's nightly release packages
         # for now, we can only run tests on gfx94X since we only have a linux gfx94X test machine
-        if TODAY_DATE in asset_name and "gfx94X" in asset_name:
+        if today_date in asset_name and "gfx94X" in asset_name:
             target_info = amdgpu_family_info_matrix.get("gfx94X").get("linux")
             target_info["file_name"] = asset_name
             release_matrix.append(target_info)
 
-    set_github_output(
-        {
-            "release_matrix": json.dumps(release_matrix),
-            "today_date": TODAY_DATE,
-        }
-    )
+    set_github_output({"release_matrix": json.dumps(release_matrix)})
 
 
 if __name__ == "__main__":
     args = {}
     args["asset_files"] = os.environ.get("ASSET_FILES", "[]")
+    args["today_date"] = os.environ.get("TODAY_DATE", "")
     main(args)
