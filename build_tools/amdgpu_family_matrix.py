824765815134e83746da17c4f1c1711c49d8a256
geomin12 <geomin12@amd.com>
2025-04-10 13:27:53 -0700

    Initial combination of matrix use

---
+++ build_tools/amdgpu_family_matrix.py
@@ -0,0 +1,38 @@
+amdgpu_family_info_matrix = {
+    "gfx94X": {
+        "linux": {
+            "test-runs-on": "linux-mi300-1gpu-ossci-rocm",
+            "target": "gfx94X-dcgpu",
+        }
+    },
+    "gfx110X": {
+        "linux": {
+            "test-runs-on": "",
+            "target": "gfx110X-dgpu",
+        },
+        "windows": {
+            "test-runs-on": "",
+            "target": "gfx110X-dgpu",
+        },
+    },
+    "gfx115X": {
+        "linux": {
+            "test-runs-on": "",
+            "target": "gfx115X-igpu",
+        },
+        "windows": {
+            "test-runs-on": "",
+            "target": "gfx115X-igpu",
+        },
+    },
+    "gfx120X": {
+        "linux": {
+            "test-runs-on": "",
+            "target": "gfx120X-all",
+        },
+        "windows": {
+            "test-runs-on": "",
+            "target": "gfx120X-all",
+        },
+    }
+}
---
+++ build_tools/fetch_package_targets.py
@@ -0,0 +1,18 @@
+import os
+import json
+from configure_ci import set_github_output
+from amdgpu_family_matrix import amdgpu_family_info_matrix
+
+# This file helps generate a package target matrix for portable_linux_package_matrix.yml and publish_pytorch_dev_docker.yml
+
+def main():
+    package_targets = []
+    for key in amdgpu_family_info_matrix:
+        target = amdgpu_family_info_matrix.get(key).get("linux").get("target")
+        package_targets.append({"amdgpu_family": target})
+
+    set_github_output({"package_targets": json.dumps(package_targets)})
+
+
+if __name__ == "__main__":
+    main()
