08b28382f89e3246508e76225e3429f067eb2c31
erman-gurses <erman.gurses@amd.com>
2025-04-28 11:17:33 -0500

    Add as default value

--- dockerfiles/pytorch-dev/install_rocm_from_tarballs.sh
+++ dockerfiles/pytorch-dev/install_rocm_from_tarballs.sh
@@ -35,7 +35,7 @@ RELEASE_TAG="${RELEASE_TAG:-nightly-release}"
 ROCM_VERSION_DATE="${ROCM_VERSION_DATE:-$(date -d '3 days ago' +'%Y%m%d')}"
 
 # Read only the base version (like "6.4.0") from version.json
-VERSION_JSON_PATH="${VERSION_JSON_PATH: version.json}"
+: "${VERSION_JSON_PATH:="version.json"}"
 
 if [[ ! -f "$VERSION_JSON_PATH" ]]; then
   echo "[ERROR] version.json not found at $VERSION_JSON_PATH"
@@ -44,6 +44,7 @@ fi
 
 ROCM_VERSION=$(jq -r '.["rocm-version"]' "$VERSION_JSON_PATH")
 ROCM_VERSION_PREFIX="${ROCM_VERSION}rc"
+
 
 INSTALL_PREFIX="${INSTALL_PREFIX:-/therock/build/dist/rocm}"
 OUTPUT_ARTIFACTS_DIR="${OUTPUT_ARTIFACTS_DIR:-/rocm-tarballs}"
