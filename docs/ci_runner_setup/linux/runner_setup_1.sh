ad34fe5e0a94c439e823baebf2576d912f2da376
geomin12 <geomin12@amd.com>
2025-04-29 16:48:00 -0700

    Adding docs

---
+++ docs/ci_runner_setup/linux/runner_setup_1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# runner setup
+mkdir "actions-runner-$1" && cd "actions-runner-$1"
+curl -o actions-runner-linux-x64-2.323.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.323.0/actions-runner-linux-x64-2.323.0.tar.gz
+echo "0dbc9bf5a58620fc52cb6cc0448abcca964a8d74b5f39773b7afcad9ab691e19  actions-runner-linux-x64-2.323.0.tar.gz" | shasum -a 256 -c
+tar xzf ./actions-runner-linux-x64-2.323.0.tar.gz
