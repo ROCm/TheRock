b450110d978f1832f45b5f686d1467891258124a
geomin12 <geomin12@amd.com>
2025-05-05 09:10:23 -0700

    Moving pip install

--- build_tools/detail/linux_portable_build_in_container.sh
+++ build_tools/detail/linux_portable_build_in_container.sh
@@ -5,8 +5,6 @@ set -e
 set -o pipefail
 trap 'kill -TERM 0' INT
 
-pip install -r /therock/src/requirements.txt
-
 OUTPUT_DIR="/therock/output"
 mkdir -p "$OUTPUT_DIR/caches"
 
@@ -14,6 +12,8 @@ export CCACHE_DIR="$OUTPUT_DIR/caches/container/ccache"
 export PIP_CACHE_DIR="$OUTPUT_DIR/caches/container/pip"
 mkdir -p "$CCACHE_DIR"
 mkdir -p "$PIP_CACHE_DIR"
+
+pip install -r /therock/src/requirements.txt
 
 export CMAKE_C_COMPILER_LAUNCHER=ccache
 export CMAKE_CXX_COMPILER_LAUNCHER=ccache
