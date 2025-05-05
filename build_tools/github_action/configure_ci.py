9dfaa1c4536321e9dd4b2db31aff64ec6f844873
geomin12 <geomin12@amd.com>
2025-05-05 10:57:21 -0700

    Adding outputs

--- build_tools/github_action/configure_ci.py
+++ build_tools/github_action/configure_ci.py
@@ -28,8 +28,10 @@
 
   Written to GITHUB_OUTPUT:
   * linux_amdgpu_families : List of valid Linux AMD GPU families to execute build and test jobs
-  * build_windows_amdgpu_families : List of valid Windows AMD GPU families to execute build jobs
-  * test_windows_amdgpu_families : List of valid Windows AMD GPU families to execute test jobs
+  * run_linux_tests_only: If enabled, CI will only run Linux tests
+  * windows_amdgpu_families : List of valid Windows AMD GPU families to execute build and test jobs
+  * run_windows_tests_only: If enabled, CI will only run Windows tests
+  * enable_build_jobs: If true, builds will be enabled
 
   Written to GITHUB_STEP_SUMMARY:
   * Human-readable summary for most contributors
@@ -329,7 +331,9 @@ def main(base_args, linux_families, windows_families):
 
     output = {
         "linux_amdgpu_families": json.dumps(linux_target_output),
+        "run_linux_tests_only": json.dumps(base_args.get("run_linux_tests_only")),
         "windows_amdgpu_families": json.dumps(windows_target_output),
+        "run_windows_tests_only": json.dumps(base_args.get("run_windows_tests_only")),
         "enable_build_jobs": json.dumps(enable_build_jobs),
     }
     set_github_output(output)
