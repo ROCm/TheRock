1957aac1757e71f1bdf9f7b4055781a00d776fce
erman-gurses <erman.gurses@amd.com>
2025-05-20 12:24:40 -0700

    Use GPU as a keyword for rocminfo

--- external-builds/pytorch/smoke-tests/run_smoke_tests.sh
+++ external-builds/pytorch/smoke-tests/run_smoke_tests.sh
@@ -10,7 +10,7 @@ echo 'rocm-smi'
 rocm-smi
 #  Check for ROCm GPU availability
 echo 'Checking for ROCm-compatible GPU...'
-if ! rocminfo | grep -qi 'Name: .*AMD'; then
+if ! rocminfo | grep -q 'Device Type: *GPU'; then
   echo "ERROR: No ROCm-compatible GPU detected."
   exit 1
 fi
