33b799cb08f80d8bc3ccb2f610848ca00fb426da
Ubuntu <sosa@sosa-ubuntu-new.jlrpb3anfk0e1lsq5c0tiw3fjg.cx.internal.cloudapp.net>
2025-05-21 23:11:11 +0000

    Address Scott's comments

--- build_tools/hack/delete_workflow.sh
+++ build_tools/hack/delete_workflow.sh
@@ -7,46 +7,49 @@
 # Usage: delete-workflow.sh <org/repo> <workflow-name (aka path to workflow
 # file e.g. blahblah.yml )>
 #
+# PREREQUISITES: Install https://github.com/cli/cli and authenticate with it.
+#
+#
 # Inspired by and mostly copied from:
 # https://github.com/orgs/community/discussions/26256
 
 set -oe pipefail
 
-REPOSITORY=$1
-WORKFLOW_NAME=$2
+REPOSITORY=${1}
+WORKFLOW_NAME=${2}
 
 # Validate arguments
-if [[ -z "$REPOSITORY" ]]; then
+if [[ -z "${REPOSITORY}" ]]; then
   echo "Repository is required"
   exit 1
 fi
 
-if [[ -z "$WORKFLOW_NAME" ]]; then
+if [[ -z "${WORKFLOW_NAME}" ]]; then
   echo "Workflow name is required"
   exit 1
 fi
 
-echo "Getting all completed runs for workflow $WORKFLOW_NAME in $REPOSITORY"
+echo "Getting all completed runs for workflow ${WORKFLOW_NAME} in ${REPOSITORY}"
 
-RUNS=$(
+RUNS=${(
   gh api \
     -H "Accept: application/vnd.github+json" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    "/repos/$REPOSITORY/actions/workflows/$WORKFLOW_NAME/runs" \
+    "/repos/${REPOSITORY}/actions/workflows/${WORKFLOW_NAME}/runs" \
     --paginate \
     --jq '.workflow_runs[] | select(.conclusion != "") | .id'
 ) || RUNS=0
 
-if [[ $RUNS -eq 0 ]]; then
-  echo "No runs found for $WORKFLOW_NAME in $REPOSITORY"
+if [[ ${RUNS -eq 0 ]]; then
+  echo "No runs found for ${WORKFLOW_NAME} in ${REPOSITORY}"
   exit
 fi
 
-echo "Found $(echo "$RUNS" | wc -l) completed runs for workflow $WORKFLOW_NAME"
+echo "Found $(echo '${RUNS}' | wc -l) completed runs for workflow ${WORKFLOW_NAME}"
 
 echo "Would you like to continue:"
 select answer in "yes" "no"; do
-  case $answer in
+  case ${answer} in
     yes)
       break
       ;;
@@ -56,7 +59,7 @@ select answer in "yes" "no"; do
   esac
 done
 
-for RUN in $RUNS; do
-  gh run delete --repo "$REPOSITORY" "$RUN" || echo "Failed to delete run $RUN"
+for RUN in ${RUNS}; do
+  gh run delete --repo "${REPOSITORY}" "${RUN}" || echo "Failed to delete run ${RUN}"
   sleep 0.1
 done
