353f95f1b0e071f1597f835ac0a2e1e7f0629221
geomin12 <geomin12@amd.com>
2025-05-30 14:56:37 -0700

    Pr comments

--- build_tools/github_actions/artifact_upload.py
+++ build_tools/github_actions/artifact_upload.py
@@ -13,6 +13,20 @@ THEROCK_DIR = Path(__file__).resolve().parent.parent.parent
 GENERIC_VARIANT = "generic"
 PLATFORM = platform.system().lower()
 
+# Importing indexer.py
+sys.append(THEROCK_DIR / "third-party" / "indexer")
+from indexer import process_dir
+
+# Importing create_log_index.py and upload_logs_to_s3.py
+sys.append(THEROCK_DIR / "build_tools")
+from create_log_index import index_log_files
+from upload_logs_to_s3 import upload_logs_to_s3
+
+
+def exec(cmd):
+    logging.info(f"Executing cmd {shlex.join(cmd)}")
+    subprocess.run(cmd, check=True)
+
 
 def retrieve_bucket_info():
     GITHUB_REPOSITORY = os.getenv("GITHUB_REPOSITORY", "ROCm/TheRock")
@@ -37,28 +51,19 @@ def set_github_step_summary(summary: str):
 
 def create_index_file(args: argparse.Namespace):
     logging.info("Creating index file")
-    index_file_path = THEROCK_DIR / "third-party" / "indexer" / "indexer.py"
-    build_dir = args.build_dir
+    build_dir = args.build_dir / "artifacts"
 
-    subprocess.run(
-        [sys.executable, index_file_path, "-f", "*.tar.xz*", build_dir / "artifacts"]
-    )
+    indexer_args = argparse.Namespace()
+    indexer_args.filter = "*.tar.xz*"
+    process_dir(build_dir, indexer_args)
 
 
 def create_log_index(args: argparse.Namespace):
     logging.info("Creating log index file")
-    create_log_index_path = THEROCK_DIR / "build_tools" / "create_log_index.py"
     build_dir = args.build_dir
     amdgpu_family = args.amdgpu_family
 
-    subprocess.run(
-        [
-            sys.executable,
-            create_log_index_path,
-            f"--build-dir={str(build_dir)}",
-            f"--amdgpu-family={amdgpu_family}",
-        ]
-    )
+    index_log_files(build_dir, amdgpu_family)
 
 
 def upload_artifacts(args: argparse.Namespace, bucket_uri: str):
@@ -80,8 +85,7 @@ def upload_artifacts(args: argparse.Namespace, bucket_uri: str):
         "--include",
         "*.tar.xz*",
     ]
-    logging.info(f"Executing cmd {shlex.join(cmd)}")
-    subprocess.run(cmd)
+    exec(cmd)
 
     # Uploading index.html to S3 bucket
     cmd = [
@@ -91,31 +95,21 @@ def upload_artifacts(args: argparse.Namespace, bucket_uri: str):
         str(build_dir / "artifacts" / "index.html"),
         f"{bucket_uri}/index-{amdgpu_family}.html",
     ]
-    logging.info(f"Executing cmd {shlex.join(cmd)}")
-    subprocess.run(cmd)
+    exec(cmd)
 
 
 def upload_logs(args: argparse.Namespace, bucket: str, bucket_uri: str):
     logging.info(f"Uploading logs to S3 for bucket {bucket}")
     build_dir = args.build_dir
     amdgpu_family = args.amdgpu_family
-    upload_logs_s3_path = THEROCK_DIR / "build_tools" / "upload_logs_to_s3.py"
     s3_base_path = f"{bucket_uri}/logs/{amdgpu_family}"
 
-    subprocess.run(
-        [
-            sys.executable,
-            upload_logs_s3_path,
-            f"--build-dir={build_dir}",
-            f"--s3-base-path={s3_base_path}",
-        ]
-    )
+    upload_logs_to_s3(s3_base_path, build_dir)
 
 
 def add_links_to_job_summary(args: argparse.Namespace, bucket: str, bucket_url: str):
     logging.info(f"Adding links to job summary to bucket {bucket}")
     build_dir = args.build_dir
-    run_id = args.run_id
     amdgpu_family = args.amdgpu_family
 
     log_url = f"{bucket_url}/logs/{amdgpu_family}/index.html"
