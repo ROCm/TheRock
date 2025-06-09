95f58e13b32df3882530a498a96603b201b22f8f
Aravind Ravikumar <60629247+araravik-psd@users.noreply.github.com>
2025-06-09 18:07:27 -0400

    Create fetch_job_status.py

---
+++ build_tools/github_actions/fetch_job_status.py
@@ -0,0 +1,48 @@
+"""
+This script determines the job status for different jobs run as part of GitHub workflow
+based on if the specific release body contains today's version.
+
+Required environment variables:
+  - TAG_NAME
+  - VERSION
+"""
+
+from configure_ci import set_github_output
+import json
+import os
+from urllib.request import urlopen, Request
+
+TAG_NAME = os.getenv("TAG_NAME")
+VERSION = os.getenv("VERSION")
+
+
+def run():
+    github_release_url = (
+        f"https://api.github.com/repos/RoCm/TheRock/actions/runs/${{ github.run_id }}/attempts/${{ github.attempt }}/jobs"
+    )
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+    # If GITHUB_TOKEN environment variable is available, include it in the API request to avoid a lower rate limit
+    gh_token = os.getenv("GITHUB_TOKEN", "")
+    if gh_token:
+        headers["Authentication"] = f"Bearer {gh_token}"
+
+    request = Request(github_release_url, headers=headers)
+    with urlopen(request) as response:
+        if response.status == 403:
+            raise Exception(
+                f"Error when retrieving GitHub release assets for release tag '{TAG_NAME}'. This is most likely a rate limiting issue, so please try again"
+            )
+        elif response.status != 200:
+            raise Exception(
+                f"Error when retrieving GitHub release assets for release tag '{TAG_NAME}' with status code {response.status}. Exiting..."
+            )
+
+        job_data = json.loads(response.read().decode("utf-8"))
+        echo $job_data
+
+
+if __name__ == "__main__":
+    run()
