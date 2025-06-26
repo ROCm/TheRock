ad5455b66d977a7dbfa2727142a1a2fe44d9811b
geomin12 <geomin12@amd.com>
2025-06-26 16:55:29 -0700

    Trying this;

--- build_tools/github_actions/fetch_test_configurations.py
+++ build_tools/github_actions/fetch_test_configurations.py
@@ -17,8 +17,8 @@ import os
 from pathlib import Path
 from configure_ci import set_github_output
 
-
-THEROCK_BIN_DIR = os.getenv("THEROCK_BIN_DIR")
+THIS_SCRIPT_DIR = Path(__file__).resolve().parent
+THEROCK_BIN_DIR = str(THIS_SCRIPT_DIR.parent.parent / "build/bin")
 
 test_matrix = {
     # BLAS tests
