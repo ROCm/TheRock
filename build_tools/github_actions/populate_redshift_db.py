35270ef64498ca2323dda4df322d64cf154afa40
arravikum <arravikum@amd.com>
2025-06-30 19:37:25 +0000

    Changed Cluster passwd

--- build_tools/github_actions/populate_redshift_db.py
+++ build_tools/github_actions/populate_redshift_db.py
@@ -2,12 +2,6 @@ import os
 import psycopg2
 import argparse
 import json
-# # Load credentials from environment
-# host = 'therockcluster-1.cgwwfykpw9ix.us-east-2.redshift.amazonaws.com'
-# port = 5439
-# dbname = "workflow_database"
-# user = "awsuser"
-# password = "7mP+a[61{tIFDK!0"
 
 parser = argparse.ArgumentParser(description="Populate DB in redshift cluster")
 parser.add_argument(
@@ -51,11 +45,9 @@ args = parser.parse_args()
 
 build_id = args.build_id
 
-# build_id = 306
-# args.input = '{"total_count": 1, "jobs": [{"id": 44954385538, "run_id": 15935530212, "workflow_name": "Build Linux Packages", "head_branch": "users/arravikum/workflow_analysis", "run_url": "https://api.github.com/repos/ROCm/TheRock/actions/runs/15935530212", "run_attempt": 1, "node_id": "CR_kwDOLaI0488AAAAKd318gg", "head_sha": "a8978d7d0a8cd86e7c49a308cb92d119dac8f4e7", "url": "https://api.github.com/repos/ROCm/TheRock/actions/jobs/44954385539", "html_url": "https://github.com/ROCm/TheRock/actions/runs/15935530212/job/44954385539", "status": "in_progress", "conclusion": null, "created_at": "2025-06-27T20:39:11Z", "started_at": "2025-06-27T20:39:34Z", "completed_at": null, "name": "Build Linux Packages (xfail false)", "steps": [{"name": "Set up job", "status": "completed", "conclusion": "success", "number": 1, "started_at": "2025-06-27T20:39:35Z", "completed_at": "2025-06-27T20:39:36Z"}, {"name": "Initialize containers", "status": "completed", "conclusion": "success", "number": 2, "started_at": "2025-06-27T20:39:36Z", "completed_at": "2025-06-27T20:40:18Z"}, {"name": "Checking out repository", "status": "completed", "conclusion": "success", "number": 3, "started_at": "2025-06-27T20:40:18Z", "completed_at": "2025-06-27T20:40:19Z"}, {"name": "Configure AWS Credentials", "status": "completed", "conclusion": "success", "number": 4, "started_at": "2025-06-27T20:40:19Z", "completed_at": "2025-06-27T20:40:19Z"}, {"name": "Create Logs index Files", "status": "completed", "conclusion": "success", "number": 5, "started_at": "2025-06-27T20:40:19Z", "completed_at": "2025-06-27T20:40:19Z"}, {"name": "Determine job status for workflow run", "status": "in_progress", "conclusion": null, "number": 6, "started_at": "2025-06-27T20:40:19Z", "completed_at": null}, {"name": "Create job status JSON file for S3 upload", "status": "pending", "conclusion": null, "number": 7, "started_at": null, "completed_at": null}, {"name": "Install psycopg2", "status": "pending", "conclusion": null, "number": 8, "started_at": null, "completed_at": null}, {"name": "Populate tables in redshift", "status": "pending", "conclusion": null, "number": 9, "started_at": null, "completed_at": null}, {"name": "Upload Logs", "status": "pending", "conclusion": null, "number": 10, "started_at": null, "completed_at": null}, {"name": "Post Configure AWS Credentials", "status": "pending", "conclusion": null, "number": 18, "started_at": null, "completed_at": null}, {"name": "Post Checking out repository", "status": "pending", "conclusion": null, "number": 19, "started_at": null, "completed_at": null}, {"name": "Stop containers", "status": "pending", "conclusion": null, "number": 20, "started_at": null, "completed_at": null}], "check_run_url": "https://api.github.com/repos/ROCm/TheRock/check-runs/44954385538", "labels": ["azure-linux-scale-rocm"], "runner_id": 84105, "runner_name": "azure-linux-scale-rocm-76pqr-runner-x9dqp", "runner_group_id": 1, "runner_group_name": "default"}]}'
 input_dict = json.loads(args.api_op)
 
-print(args.input)
+print(args.api_op)
 
 conn = psycopg2.connect(
     host=redshift_cluster_endpoint,
