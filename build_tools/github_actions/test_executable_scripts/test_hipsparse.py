6db02b525197a3c01fd8f51cf11401e77bcd03d7
geomin12 <geomin12@amd.com>
2025-07-07 20:27:12 -0700

    Adding sparse tests

---
+++ build_tools/github_actions/test_executable_scripts/test_hipsparse.py
@@ -0,0 +1,23 @@
+import logging
+import os
+import shlex
+import subprocess
+from pathlib import Path
+
+THEROCK_BIN_DIR = os.getenv("THEROCK_BIN_DIR")
+OUTPUT_ARTIFACTS_DIR = os.getenv("OUTPUT_ARTIFACTS_DIR")
+SCRIPT_DIR = Path(__file__).resolve().parent
+THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
+
+logging.basicConfig(level=logging.INFO)
+
+cmd = [
+    f"HIPSPARSE_CLIENTS_MATRICES_DIR={OUTPUT_ARTIFACTS_DIR}/clients/matrices",
+    f"{THEROCK_BIN_DIR}/hipsparse-test",
+]
+logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmd)}")
+subprocess.run(
+    cmd,
+    cwd=THEROCK_DIR,
+    check=True,
+)
---
+++ build_tools/github_actions/test_executable_scripts/test_rocsparse.py
@@ -0,0 +1,24 @@
+import logging
+import os
+import shlex
+import subprocess
+from pathlib import Path
+
+THEROCK_BIN_DIR = os.getenv("THEROCK_BIN_DIR")
+OUTPUT_ARTIFACTS_DIR = os.getenv("OUTPUT_ARTIFACTS_DIR")
+SCRIPT_DIR = Path(__file__).resolve().parent
+THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
+
+logging.basicConfig(level=logging.INFO)
+
+cmd = [
+    f"ROCSPARSE_CLIENTS_MATRICES_DIR={OUTPUT_ARTIFACTS_DIR}/clients/matrices",
+    f"{THEROCK_BIN_DIR}/rocsparse-test",
+    "--gtest_filter=*pre_checkin*",
+]
+logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmd)}")
+subprocess.run(
+    cmd,
+    cwd=THEROCK_DIR,
+    check=True,
+)
