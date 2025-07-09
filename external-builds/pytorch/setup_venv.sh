51c7066c48f69f0f5f28652e97e0b1c22371275f
Marius Brehler <marius.brehler@amd.com>
2025-07-09 21:53:37 +0000

    Rework `test_pytorch_wheels` workflow
    * Moves venv creation to `setup_venv.sh`, it should not be handled in
      `run_linux_pytorch_tests.sh`.
    * Do not longer install packages in `run_linux_pytorch_tests.py` as
      these are installed in `setup_venv.sh`.
    * Cleans up the description in `pytorch_torch_repo.py`.
    * Moves environment variables.
    * Modifies the workflow and script to install a specific torch version.

--- external-builds/pytorch/setup_venv.sh
+++ external-builds/pytorch/setup_venv.sh
@@ -10,11 +10,24 @@ VENV_DIR="${1:-$(realpath "$(dirname "${BASH_SOURCE[0]}")/../..")/venv}"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REQUIREMENTS_FILE="${SCRIPT_DIR}/requirements-test.txt"
 
-# Create virtual environment
+# Ensure INDEX_URL is set
+if [[ -z "${INDEX_URL:-}" ]]; then
+  echo "ERROR: INDEX_URL environment variable must be set"
+  exit 1
+fi
+
+# Ensure TORCH_VERSION is set
+if [[ -z "${TORCH_VERION:-}" ]]; then
+  echo "ERROR: TORCH_VERSION environment variable must be set"
+  exit 1
+fi
+
+# Create and activate virtual environment
 python -m venv "$VENV_DIR"
+source "$VENV_DIR/bin/activate"
 
 # Install dependencies
-"$VENV_DIR/bin/pip" install --upgrade pip
-"$VENV_DIR/bin/pip" install -r "$REQUIREMENTS_FILE"
+pip install --upgrade pip
+pip install -r "$REQUIREMENTS_FILE"
 # Install torch, overwriting any previously installed versions.
-"$VENV_DIR/bin/pip" install --index-url "$INDEX_URL" torch --no-cache-dir --force-reinstall
+pip install --index-url "${INDEX_URL}" torch==${TORCH_VERSION} --no-cache-dir --force-reinstall
