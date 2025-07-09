9e5f2d1bda54091694505fcba566fcbcf0aa5a08
Marius Brehler <marius.brehler@amd.com>
2025-07-09 22:48:52 +0000

    Address review comments

--- external-builds/pytorch/setup_venv.sh
+++ external-builds/pytorch/setup_venv.sh
@@ -10,18 +10,6 @@ VENV_DIR="${1:-$(realpath "$(dirname "${BASH_SOURCE[0]}")/../..")/venv}"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REQUIREMENTS_FILE="${SCRIPT_DIR}/requirements-test.txt"
 
-# Ensure INDEX_URL is set
-if [[ -z "${INDEX_URL:-}" ]]; then
-  echo "ERROR: INDEX_URL environment variable must be set"
-  exit 1
-fi
-
-# Ensure TORCH_VERSION is set
-if [[ -z "${TORCH_VERION:-}" ]]; then
-  echo "ERROR: TORCH_VERSION environment variable must be set"
-  exit 1
-fi
-
 # Create and activate virtual environment
 python -m venv "$VENV_DIR"
 source "$VENV_DIR/bin/activate"
@@ -30,4 +18,4 @@ source "$VENV_DIR/bin/activate"
 pip install --upgrade pip
 pip install -r "$REQUIREMENTS_FILE"
 # Install torch, overwriting any previously installed versions.
-pip install --index-url "${INDEX_URL}" torch==${TORCH_VERSION} --no-cache-dir --force-reinstall
+pip install --index-url "${INDEX_URL?}" torch==${TORCH_VERSION?} --no-cache-dir --force-reinstall
