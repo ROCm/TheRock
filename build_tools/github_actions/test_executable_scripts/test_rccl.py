033c6e56146c9b0de3e3eae9531c15ceba11dd94
geomin12 <geomin12@amd.com>
2025-07-15 09:51:52 -0700

    Adding more RCCL tests

--- build_tools/github_actions/test_executable_scripts/test_rccl.py
+++ build_tools/github_actions/test_executable_scripts/test_rccl.py
@@ -10,6 +10,7 @@ THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
 
 logging.basicConfig(level=logging.INFO)
 
+# Executing rccl gtest from rccl repo
 cmd = [f"{THEROCK_BIN_DIR}/rccl-UnitTests"]
 logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmd)}")
 subprocess.run(
@@ -17,3 +18,27 @@ subprocess.run(
     cwd=THEROCK_DIR,
     check=True,
 )
+
+# Executing rccl performance and correctness tests from rccl-tests repo
+executables = [
+    "all_gather_perf",
+    "alltoallv_perf",
+    "broadcast_perf",
+    "alltoall_perf",
+    "all_reduce_perf",
+    "reduce_perf",
+    "hypercube_perf",
+    "gather_perf",
+    "scatter_perf",
+    "sendrecv_perf",
+    "reduce_scatter_perf",
+]
+
+for executable in executables:
+    cmd = [f"{THEROCK_BIN_DIR}/{executable}"]
+    logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmd)}")
+    subprocess.run(
+        cmd,
+        cwd=THEROCK_DIR,
+        check=True,
+    )
