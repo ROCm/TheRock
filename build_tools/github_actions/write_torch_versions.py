48ab8a916aa92e3c63b7190d4ed3c5793715a5d6
arravikum <arravikum@amd.com>
2025-07-25 01:45:49 +0000

    Added torchvision and torchaudio versions in write_torch_version.py and added specific tested Torch version aws cp

---
+++ build_tools/github_actions/write_torch_versions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""Writes torch_version to GITHUB_OUTPUT."""
+
+import os
+import glob
+
+from github_actions_utils import *
+
+
+def main(argv: list[str]):
+    # Get the torch version from the first torch wheel in PACKAGE_DIST_DIR.
+    package_dist_dir = os.getenv("PACKAGE_DIST_DIR")
+    torch_version = glob.glob("torch-*.whl", root_dir=package_dist_dir)[0].split("-")[1]
+    gha_set_output({"torch_version": torch_version})
+
+    torchaudio_version = glob.glob("torchaudio-*.whl", root_dir=package_dist_dir)[
+        0
+    ].split("-")[1]
+    gha_set_output({"torchaudio_version": torchaudio_version})
+
+    torchvideo_version = glob.glob("torchvideo-*.whl", root_dir=package_dist_dir)[
+        0
+    ].split("-")[1]
+    gha_set_output({"torchaudio_version": torchvideo_version})
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
