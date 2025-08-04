2d44df6532a05282af69d48e450912f634e96df5
Vsevolod Golovko <gvsevolo@amd.com>
2025-08-04 16:01:05 +0000

    Add BN tests for running

--- build_tools/github_actions/test_executable_scripts/test_miopen.py
+++ build_tools/github_actions/test_executable_scripts/test_miopen.py
@@ -10,7 +10,31 @@ THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
 
 logging.basicConfig(level=logging.INFO)
 
-cmd = [f"{THEROCK_BIN_DIR}/miopen_gtest", "--gtest_filter=*Fusion*"]
+###########################################
+
+positive_filter=[]
+negative_filter=[]
+
+# Fusion #
+positive_filter.append("*Fusion*")
+
+# Batch Normalization #
+positive_filter.append("*_BNBWD*_*")
+positive_filter.append("*_BNOCLBWD*_*")
+positive_filter.append("*_BNFWD*_*")
+positive_filter.append("*_BNOCLFWD*_*")
+positive_filter.append("*_BNInfer*_*")
+positive_filter.append("*_BNOCLInfer*_*")
+positive_filter.append("*_bn_infer*_*")
+
+negative_filter.append("*_BN*Large*_*")
+negative_filter.append("*_BN*SerialRun*_*")
+
+gtest_final_filter_cmd = "--gtest_filter=" + ':'.join(positive_filter) + "-" + ':'.join(negative_filter)
+
+#############################################
+
+cmd = [f"{THEROCK_BIN_DIR}/miopen_gtest", gtest_final_filter_cmd]
 logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmd)}")
 subprocess.run(
     cmd,
