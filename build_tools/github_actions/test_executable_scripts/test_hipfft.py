9463e7af94d34159d2006cbee1d0073f8d4e0f96
geomin12 <geomin12@amd.com>
2025-08-26 19:55:44 -0700

    trying new flag

--- build_tools/github_actions/test_executable_scripts/test_hipfft.py
+++ build_tools/github_actions/test_executable_scripts/test_hipfft.py
@@ -12,8 +12,8 @@ logging.basicConfig(level=logging.INFO)
 
 cmd = [
     f"{THEROCK_BIN_DIR}/hipfft-test",
-    # "--gtest_filter=-*multi_gpu*",
-    "--precompile=rocfft-test-precompile.db",
+    "--gtest_filter=-*multi_gpu*",
+    # "--precompile=rocfft-test-precompile.db",
 ]
 logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmd)}")
 subprocess.run(
