dc4f1439a3283a1426e9d960fdd6ea6e196e64ce
geomin12 <geomin12@amd.com>
2025-09-25 16:42:34 -0700

    Adding exclusion inside script

--- build_tools/github_actions/test_executable_scripts/test_rocsparse.py
+++ build_tools/github_actions/test_executable_scripts/test_rocsparse.py
@@ -9,6 +9,9 @@ OUTPUT_ARTIFACTS_DIR = os.getenv("OUTPUT_ARTIFACTS_DIR")
 SCRIPT_DIR = Path(__file__).resolve().parent
 THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
 
+PLATFORM = os.getenv("PLATFORM")
+AMDGPU_FAMILIES = os.getenv("AMDGPU_FAMILIES")
+
 logging.basicConfig(level=logging.INFO)
 
 # GTest sharding
@@ -19,9 +22,28 @@ envion_vars = os.environ.copy()
 envion_vars["GTEST_SHARD_INDEX"] = str(int(SHARD_INDEX) - 1)
 envion_vars["GTEST_TOTAL_SHARDS"] = str(TOTAL_SHARDS)
 
+exclude_tests_filter = ""
+
+tests_to_exclude = {
+    # Related issue: https://ontrack-internal.amd.com/browse/SWDEV-557164
+    "gfx1151": {
+        "windows": [
+            "quick/bsrsv.level2/*",
+            "quick/coomv.level2/*",
+            "quick/csrmv.level2/*",
+        ]
+    }
+}
+
+if (
+    AMDGPU_FAMILIES in tests_to_exclude
+    and PLATFORM in tests_to_exclude[AMDGPU_FAMILIES]
+):
+    exclude_tests_filter += "-" + ":".join(tests_to_exclude[AMDGPU_FAMILIES][PLATFORM])
+
 cmd = [
     f"{THEROCK_BIN_DIR}/rocsparse-test",
-    "--gtest_filter=*quick*",
+    "--gtest_filter=*quick*" + exclude_tests_filter,
     "--matrices-dir",
     f"{OUTPUT_ARTIFACTS_DIR}/clients/matrices/",
 ]
