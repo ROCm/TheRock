5003955a647cccb477aada418fcbe2f8157ee3d3
geomin12 <geomin12@amd.com>
2025-10-01 16:49:56 -0700

    Removing lone s

--- build_tools/github_actions/amdgpu_family_matrix.py
+++ build_tools/github_actions/amdgpu_family_matrix.py
@@ -43,7 +43,7 @@ amdgpu_family_info_matrix_postsubmit = {
     "gfx950": {
         "linux": {
             # Networking issue: https://github.com/ROCm/TheRock/issues/1660
-            # Label is "linux-mi355-1gpu-ossci-rocm"s
+            # Label is "linux-mi355-1gpu-ossci-rocm"
             "test-runs-on": "",
             "family": "gfx950-dcgpu",
         }
