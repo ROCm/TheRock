8cde7ff2965b157546d55580190e7ef0528d64d8
geomin12 <geomin12@amd.com>
2025-10-07 10:24:41 -0700

    fixing scripts

--- build_tools/github_actions/fetch_test_configurations.py
+++ build_tools/github_actions/fetch_test_configurations.py
@@ -142,6 +142,7 @@ test_matrix = {
         "test_script": f"python {_get_script_path('test_rocfft.py')}",
         # TODO(geomin12): Add windows test (https://github.com/ROCm/TheRock/issues/1391)
         "platform": ["linux"],
+        "total_shards": 1,
     },
     "hipfft": {
         "job_name": "hipfft",
@@ -149,6 +150,7 @@ test_matrix = {
         "timeout_minutes": 60,
         "test_script": f"python {_get_script_path('test_hipfft.py')}",
         "platform": ["linux", "windows"],
+        "total_shards": 1,
     },
 }
 
