354ae5859704a3b2daa6d6f5a1318d45d7ab6661
geomin12 <geomin12@amd.com>
2025-10-08 11:37:38 -0700

    Adding rand dependency

--- build_tools/github_actions/fetch_test_configurations.py
+++ build_tools/github_actions/fetch_test_configurations.py
@@ -146,7 +146,7 @@ test_matrix = {
     },
     "hipfft": {
         "job_name": "hipfft",
-        "fetch_artifact_args": "--fft --tests",
+        "fetch_artifact_args": "--fft --rand --tests",
         "timeout_minutes": 60,
         "test_script": f"python {_get_script_path('test_hipfft.py')}",
         "platform": ["linux", "windows"],
