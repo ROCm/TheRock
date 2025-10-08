e9a3eb7d8cf80985dc120e98dd54febe97aea8a2
Subodh Dubey <Subodh.Dubey@amd.com>
2025-10-08 03:00:06 +0000

    HipSolver & Rocsolver Test

--- build_tools/github_actions/fetch_test_configurations.py
+++ build_tools/github_actions/fetch_test_configurations.py
@@ -66,7 +66,7 @@ test_matrix = {
     "rocsolver": {
         "job_name": "rocsolver",
         "fetch_artifact_args": "--blas --tests",
-        "timeout_minutes": 20,
+        "timeout_minutes": 15,
         "test_script": f"python {_get_script_path('test_rocsolver.py')}",
         "platform": ["linux"],
         "total_shards": 1,
