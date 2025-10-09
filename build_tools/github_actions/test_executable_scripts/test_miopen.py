99f18d7f34baad8b1737dfb02e6e082ce9c7aa59
Vsevolod Golovko <gvsevolo@amd.com>
2025-10-09 10:37:31 +0200

    temporary disable smoke mode

--- build_tools/github_actions/test_executable_scripts/test_miopen.py
+++ build_tools/github_actions/test_executable_scripts/test_miopen.py
@@ -168,15 +168,9 @@ smoke_filter = [
 
 ####################################################
 
-# If smoke tests are enabled, we run smoke tests only.
-# Otherwise, we run the normal test suite
-test_type = os.getenv("TEST_TYPE", "full")
-if test_type == "smoke":
-    test_filter = "--gtest_filter=" + ":".join(smoke_filter)
-else:
-    test_filter = (
-        "--gtest_filter=" + ":".join(positive_filter) + "-" + ":".join(negative_filter)
-    )
+test_filter = (
+    "--gtest_filter=" + ":".join(positive_filter) + "-" + ":".join(negative_filter)
+)
 
 #############################################
 
