e0598fb89088d9a95a5b9202a2d3c6421c6e0adb
Laura Promberger <laura.promberger@amd.com>
2025-10-14 13:29:50 +0000

    rename xz to liblzma and add it to linux CMakeList.txt as dep

--- third-party/sysdeps/common/xz/patch_source.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-set -e
-
-SOURCE_DIR="${1:?Source directory must be given}"
-XZ_CMAKELIST="$SOURCE_DIR/CMakeLists.txt"
-echo "Patching sources..."
-
-sed -i -E 's/(OUTPUT_NAME)[[:space:]]+z\)/\1 rocm_sysdeps_z)/' "$XZ_CMAKELIST"
