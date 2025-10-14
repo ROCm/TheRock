8332b87f5bb4a9ffe67636296950798117df5b61
Laura Promberger <laura.promberger@amd.com>
2025-10-14 12:12:19 +0000

    first dummy for adding xz as sysdeps

---
+++ third-party/sysdeps/common/xz/patch_install.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import platform
+import shutil
+import sys
+
+PREFIX = sys.argv[1]
+
+if platform.system() == "Linux":
+    source = str(Path(PREFIX) / "lib" / "librocm_sysdeps_liblzma.so")
+    destination = str(Path(PREFIX) / "lib" / "liblzma.so")
+    shutil.move(source, destination)
+    # We don't want the static lib on Linux.
+    (Path(PREFIX) / "lib" / "librocm_sysdeps_liblzma.a").unlink()
+elif platform.system() == "Windows":
+    # We don't want the .dll on Windows.
+    (Path(PREFIX) / "bin" / "liblzma.dll").unlink()
+    (Path(PREFIX) / "lib" / "liblzma.lib").unlink()
---
+++ third-party/sysdeps/common/xz/xz_include.cmake
@@ -0,0 +1,2 @@
+message(STATUS "Customizing xz/liblzma options for TheRock")
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
