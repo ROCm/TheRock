e31daee58befa19de08044196986401874757c22
Brian Harrison <brian.harrison@amd.com>
2025-10-15 00:55:05 +0000

    Fix precommit, and clean-up the cmake

--- ml-libs/post_hook_MIOpen_plugin.cmake
+++ ml-libs/post_hook_MIOpen_plugin.cmake
@@ -1,16 +1,6 @@
-# Add the plugin engines directory to the private install RPATH dirs
-# This will be picked up by therock_global_post_subproject.cmake and converted
-# to origin-relative paths for all executable and shared library targets
-
+# Add the plugin engines directory to the private install RPATH dirs for the unit tests that use the plugin.so
 list(APPEND THEROCK_PRIVATE_INSTALL_RPATH_DIRS "lib/hipdnn_plugins/engines")
 
-message(STATUS "Added lib/hipdnn_plugins/engines to THEROCK_PRIVATE_INSTALL_RPATH_DIRS for MIOpen_plugin")
-
-# The plugin library is installed in lib/hipdnn_plugins/engines/, which is 2 levels deep
-# We need to tell the build system this so it can compute correct RPATH from $ORIGIN/../../lib
-
-
-set_target_properties(miopen_legacy_plugin PROPERTIES 
+# The plugin library is installed in lib/hipdnn_plugins/engines/, and we need to set origin properly for the RPATH to work
+set_target_properties(miopen_legacy_plugin PROPERTIES
     THEROCK_INSTALL_RPATH_ORIGIN "lib/hipdnn_plugins/engines")
-    
-message(STATUS "Set THEROCK_INSTALL_RPATH_ORIGIN for miopen_legacy_plugin")
