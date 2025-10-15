2e1172f499139febb665164345d9b3c445e08b71
Marius Brehler <marius.brehler@amd.com>
2025-10-15 11:19:47 +0000

    Always install to lib

--- third-party/sysdeps/common/liblzma/liblzma_include.cmake
+++ third-party/sysdeps/common/liblzma/liblzma_include.cmake
@@ -1,5 +1,6 @@
 message(STATUS "Customizing xz/liblzma options for TheRock")
 set(CMAKE_POSITION_INDEPENDENT_CODE ON CACHE BOOL "" FORCE)
+set(CMAKE_INSTALL_LIBDIR "lib")  # No lib64 for us, thank you very much.
 
 # Force shared library build on Linux
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
