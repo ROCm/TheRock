77ac6244cb058469efe173206cbf268d1bff517e
Marius Brehler <marius.brehler@amd.com>
2025-10-15 13:29:21 +0000

    Switch to generic symbol map

--- third-party/sysdeps/common/liblzma/patch_source.sh
+++ third-party/sysdeps/common/liblzma/patch_source.sh
@@ -7,5 +7,3 @@ echo "Patching sources..."
 
 # Look in the CMakeList.txt of the project to be build what the library "OUTPUT_NAME" is
 sed -i -E 's/(OUTPUT_NAME)[[:space:]]+"lzma"/\1 "rocm_sysdeps_liblzma"/' "$LIBLZMA_CMAKELIST"
-# Ignore symbol versions. Those will get overridden with AMDROCM_SYSDEPS_1.0.
-sed -i '/target_compile_definitions(liblzma PRIVATE HAVE_SYMBOL_VERSIONS_LINUX=1)/d' "$LIBLZMA_CMAKELIST"
