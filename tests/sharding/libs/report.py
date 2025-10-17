110f4ff3d1222c098aaa397c738ce31a32583cd5
Kasula, Madhusudhan <madhusudhan.kasula@amd.com>
2025-10-17 15:33:33 +0530

    [tests-sharding] Incorporated review comments

--- tests/sharding/libs/report.py
+++ tests/sharding/libs/report.py
@@ -1,53 +1,43 @@
 #!/usr/bin/python3
-import os
-import re
-import sys
-import time
-import json
-import smtplib
 import tabulate
-import traceback
-import pymsteams
-import email.mime.text
-import email.mime.multipart
 
 from . import utils
 from libs.utils import log
 
 
 class Table():
-	''' A class to create table entries in the test report '''
-	def __init__(self, title):
-		self.title = title
-		self.data = [[]]
+    ''' A class to create table entries in the test report '''
+    def __init__(self, title):
+        self.title = title
+        self.data = [[]]
 
-	def addRow(self, *row):
-		self.data.append(list(row))
+    def addRow(self, *row):
+        self.data.append(list(row))
 
-	def pprint(self):
-		if not self.data:
-			return 'No Data Found in Report'
-		fmt = f'{self.title}\n'
-		fmt += tabulate.tabulate(self.data[1:], headers=self.data[0],
-			tablefmt='simple_outline', rowalign='center',
-		)
-		return fmt
+    def pprint(self):
+        if not self.data:
+            return 'No Data Found in Report'
+        fmt = f'{self.title}\n'
+        fmt += tabulate.tabulate(self.data[1:], headers=self.data[0],
+            tablefmt='simple_outline', rowalign='center',
+        )
+        return fmt
 
 
 class Report(object):
-	''' A class to create test reports '''
-	def __init__(self, title=''):
-		self.title = title
-		self.tables = []
+    ''' A class to create test reports '''
+    def __init__(self, title=''):
+        self.title = title
+        self.tables = []
 
-	def addTable(self, title):
-		table = Table(title)
-		self.tables.append(table)
-		return table
+    def addTable(self, title):
+        table = Table(title)
+        self.tables.append(table)
+        return table
 
-	def pprint(self):
-		log('\n')
-		log(f': {self.title} :'.center(100, '-'))
-		# tables
-		for table in self.tables:
-			log(table.pprint())
+    def pprint(self):
+        log('\n')
+        log(f': {self.title} :'.center(100, '-'))
+        # tables
+        for table in self.tables:
+            log(table.pprint())
--- tests/sharding/libs/utils.py
+++ tests/sharding/libs/utils.py
@@ -1,50 +1,113 @@
 #!/usr/bin/python3
 import os
-import re
 import sys
-import json
-import time
-import base64
-import logging
-import traceback
+import select
+import subprocess
 
 
 def _callOnce(funcPointer):
-	''' Decorator function enables calling function to get called only once per execution '''
-	def funcWrapper(*args, **kwargs):
-		if 'ret' not in funcPointer.__dict__:
-			funcPointer.ret = funcPointer(*args, **kwargs)
-		return funcPointer.ret
-	return funcWrapper
+    ''' Decorator function enables calling function to get called only once per execution '''
+    def funcWrapper(*args, **kwargs):
+        if 'ret' not in funcPointer.__dict__:
+            funcPointer.ret = funcPointer(*args, **kwargs)
+        return funcPointer.ret
+    return funcWrapper
 
 
 def log(msg, newline=True):
-	''' Common logger '''
-	if isinstance(msg, bytes):
-		msg = msg.decode('utf-8', errors='ignore')
-	msg = msg + ('', '\n')[newline]
-	sys.stdout.write(msg) and sys.stdout.flush()
+    ''' Common logger '''
+    if isinstance(msg, bytes):
+        msg = msg.decode('utf-8', errors='ignore')
+    msg = msg + ('', '\n')[newline]
+    sys.stdout.write(msg) and sys.stdout.flush()
+
+
+TIMEOUT = 1200  # default console timeout
+def runCmd(*cmd, cwd=None, env=None, stdin=None, timeout=TIMEOUT, verbose=True,
+    out=False, err=False, **kwargs,
+):
+    ''' Executes Cmd on the current node:
+        *cmd[str-varargs]: of cmd and its arguments
+        cwd[str]: current working dirpath from where cmd should run
+        env[dict]: extra environment variable to be passed to the cmd
+        stdin[str]: input to the cmd via its stdin
+        timeout[int]: min time to wait before killing the process when no activity observed
+        verbose[bool]: verbose level, True=FullLog, False=OnlyInfo-NoLog, None=NoInfo-NoLog
+    '''
+    if verbose != None:
+        cwdStr = f'cd {cwd}; ' if cwd else ''
+        envStr = ''
+        if env:
+            for key, value in env.items():
+                envStr += f"{key}='{value}' "
+        log(f'RunCmd: {cwdStr}{envStr}{" ".join(cmd)}')
+    if env:
+        env = {k:str(v) for k,v in env.items()}
+        env.update(os.environ)
+    # launch process
+    process = subprocess.Popen(cmd,
+        stdin=subprocess.PIPE if stdin else None,
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        cwd=cwd, env=env, close_fds=True, **kwargs
+    )
+    # handling stdin
+    if stdin:
+        process.stdin.write(stdin if isinstance(stdin, bytes) else stdin.encode())
+        process.stdin.close()
+    # following process stdout / stderr
+    os.set_blocking(process.stdout.fileno(), False)
+    os.set_blocking(process.stderr.fileno(), False)
+    verbose and log('out:')
+    rets, stdout, stderr = None, b'', b''
+    def _readStream(fd):
+        chunk = fd.read(8196)
+        verbose and log(chunk, newline=False)
+        return chunk
+    chunk = None
+    while chunk != b'':
+        rfds = select.select([process.stdout, process.stderr], [], [], timeout)[0]
+        if not rfds:
+            msg = f'Reached Timeout of {timeout} sec, Exiting...'
+            log(msg)
+            stdout += msg.encode()
+            process.kill()
+            break
+        if process.stdout in rfds:
+            stdout += (chunk := _readStream(process.stdout))
+        if process.stderr in rfds:
+            stderr += (chunk := _readStream(process.stderr))
+    # handling return value
+    ret = process.wait()
+    if ret != 0 and verbose != None:
+        log(f'cmd failed: {" ".join(cmd)}')
+    verbose and log(f'ret: {ret}')
+    # returns
+    if not out:
+        return ret
+    if not err:
+        return ret, (stdout+stderr).decode()
+    return ret, stdout.decode(), stderr.decode()
 
 
 def runParallel(*funcs):
-	''' Runs the given list of funcs in parallel threads and returns their respective return values
-		*funcs[(funcPtr, args, kwargs), ...]: list of funcpts along with their args and kwargs
-	'''
-	import threading
-	rets = [None] * len(funcs)
-	def proxy(i, funcPtr, *args, **kwargs):
-		rets[i] = funcPtr(*args, **kwargs)
-	# launching parallel threads
-	threads = []
-	for (i, (funcPtr, args, kwargs)) in enumerate(funcs):
-		thread = threading.Thread(target=proxy, args=(i, funcPtr, *args), kwargs=kwargs)
-		threads.append(thread)
-		thread.start()
-	# wait for threads join
-	while threads:
-		for thread in threads:
-			thread.join()
-			if thread.is_alive():
-				continue
-			threads.remove(thread)
-	return rets
+    ''' Runs the given list of funcs in parallel threads and returns their respective return values
+        *funcs[(funcPtr, args, kwargs), ...]: list of funcpts along with their args and kwargs
+    '''
+    import threading
+    rets = [None] * len(funcs)
+    def proxy(i, funcPtr, *args, **kwargs):
+        rets[i] = funcPtr(*args, **kwargs)
+    # launching parallel threads
+    threads = []
+    for (i, (funcPtr, args, kwargs)) in enumerate(funcs):
+        thread = threading.Thread(target=proxy, args=(i, funcPtr, *args), kwargs=kwargs)
+        threads.append(thread)
+        thread.start()
+    # wait for threads join
+    while threads:
+        for thread in threads:
+            thread.join()
+            if thread.is_alive():
+                continue
+            threads.remove(thread)
+    return rets
