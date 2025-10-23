ad63e9297413a476595efc018c83e4f8f8bd2473
Laura Promberger <laura.promberger@amd.com>
2025-10-23 12:15:03 +0000

    change requests

--- third-party/sysdeps/common/liblzma/liblzma_include.cmake
+++ third-party/sysdeps/common/liblzma/liblzma_include.cmake
@@ -2,7 +2,7 @@ message(STATUS "Customizing xz/liblzma options for TheRock")
 set(CMAKE_POSITION_INDEPENDENT_CODE ON CACHE BOOL "" FORCE)
 set(CMAKE_INSTALL_LIBDIR "lib")  # No lib64 for us, thank you very much.
 
-# Force shared library build on Linux
+# Only build shared library on Linux
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
-  set(BUILD_SHARED_LIBS ON CACHE BOOL "" FORCE)
+  set(BUILD_SHARED_LIBS ON)
 endif()
