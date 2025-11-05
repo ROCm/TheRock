2fcb3ea542300fa2922f37bff513c0ac59e3aa5c
Aryan Salmanpour <aryan.salmanpour@amd.com>
2025-11-05 09:23:27 -0800

    update mesa build option to build libva too

--- third-party/sysdeps/linux/amd-mesa/patch_install.sh
+++ third-party/sysdeps/linux/amd-mesa/patch_install.sh
@@ -10,5 +10,6 @@ Python3_EXECUTABLE="${Python3_EXECUTABLE:?Python3_EXECUTABLE not defined}"
 
 "$Python3_EXECUTABLE" "$THEROCK_SOURCE_DIR/build_tools/patch_linux_so.py" \
   --patchelf "${PATCHELF}" --add-prefix rocm_sysdeps_ \
-  $PREFIX/lib/dri/libgallium_drv_video.so
-  #$PREFIX/lib/dri/radeonsi_drv_video.so
+  $PREFIX/lib/libgallium_drv_video.so \
+  $PREFIX/lib/libva.so \
+  $PREFIX/lib/libva-drm.so
