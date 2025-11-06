2f57147f4baad285b2780636a623fac423456abd
Vsevolod Golovko <gvsevolo@amd.com>
2025-11-06 17:07:35 +0100

    add fixes

--- build_tools/install_rocm_from_artifacts.py
+++ build_tools/install_rocm_from_artifacts.py
@@ -176,7 +176,7 @@ def retrieve_artifacts_by_run_id(args):
         if args.miopen:
             extra_artifacts.append("miopen")
             # We need bin/MIOpenDriver executable for tests
-            extra_artifacts.append("miopen_run")
+            argv.append("miopen_run")
         if args.prim:
             extra_artifacts.append("prim")
         if args.rand:
