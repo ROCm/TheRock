a90f22b1a09a3616377e7584c266f7f76f7157bd
Justin Chen <justchen@amd.com>
2025-11-06 16:59:25 -0800

    Reintegrate writing time sync log after main merge

--- build_tools/github_actions/post_build_upload.py
+++ build_tools/github_actions/post_build_upload.py
@@ -51,6 +51,37 @@ def exec(cmd: list[str], cwd: Path):
     subprocess.run(cmd, check=True)
 
 
+# This method will output logs of the Windows Time Service and is meant
+# to help debug spurious AWS auth issues caused by time differences when
+# uploading with the AWS CLI tool. For context, see this issue and PR:
+# https://github.com/ROCm/TheRock/issues/875
+# https://github.com/ROCm/TheRock/pull/1581#issuecomment-3490177590
+def write_time_sync_log():
+    if platform.system().lower() != "windows":
+        log("[*] Current OS not windows, Skipping.")
+        return        
+
+    # Logs are from `w32tm` run in Windows HostProcess containers on Azure VMs
+    # with `/query /status` and `/stripchart /computer:time.aws.com /dataonly`
+    # and are mounted via the readonly H: drive for Github Runner Pods to access
+    startfile = Path("H:\\start.log")
+    timefile = Path("H:\\time.log")
+
+    # Only output if these files exist in the H: drive as expected on Build VMs
+    if startfile.is_file() and timefile.is_file():
+        log(f"[*] Checking time sync at: {datetime.now()}")
+
+        log("[*] Start Time Sync Log:")
+        log(startfile.read_text())
+
+        log("[*] Time Sync Log (last ~50 lines):")
+        timef = open(timefile)
+        timelines = timef.readlines()
+        log("".join(timelines[-51:]))
+    else:
+        log("[*] time.log and/or start.log not present in H:")
+
+
 def run_aws_cp(source_path: Path, s3_destination: str, content_type: str = None):
     if source_path.is_dir():
         cmd = ["aws", "s3", "cp", str(source_path), s3_destination, "--recursive"]
@@ -255,6 +286,10 @@ def run(args):
     bucket_url = (
         f"https://{bucket}.s3.amazonaws.com/{external_repo_path}{run_id}-{PLATFORM}"
     )
+
+    log("Write Windows time sync log")
+    log("----------------------")
+    write_time_sync_log()
 
     log("Upload build artifacts")
     log("----------------------")
