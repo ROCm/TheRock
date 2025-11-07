0fe88de37916a2ca13b444c1cec733ee4f9f33b1
Luis Machado <225237332+lumachad@users.noreply.github.com>
2025-11-07 10:18:04 +0000

    Add debug tools feature and ROCdbgapi to TheRock (Linux)
    * Motivation
    
    Add the debug-tools feature that will serve as the home to all debug tools
    projects we have: ROCdbgapi, ROCgdb and ROCR-debug-agent.
    
    This commit adds the feature along with ROCdbgapi.
    
    Please note this is restricted to Linux builds for now. Windows builds will
    come at a later stage.
    
    * Technical Details
    
    This commit creates the minimal set of changes required to support the ROCm
    debug tools in TheRock build platform.
    
    It adds the following:
    
    - New debug-tools features/subdirectory (updated main CMakeLists.txt)
    - Adds ROCdbgapi as a submodule using github's amd-staging branch.
    - Adds the main debug-tools/CMakeLists.txt file with instructions on how to
    build the debug tools. At the moment this is just ROCdbgapi, named amd-dbgapi.
    - Updates build_tools/fetch_sources.py
    - Updates the documentation.
    - Adds ROCdbgapi artifacts.
    
    NOTE 1: ROCdbgapi will be added as a submodule of TheRock only temporarily until
    a decision is made on the topic. It may be the case we will want to
    migrate it to rocm-systems. But adding it as a submodule now allows us to
    integrate the component into TheRock without having to wait for all the
    rocm-systems migration steps to take place.
    
    NOTE 2: To make integration and testing faster, we have a local
    amd-dbgapi.cmake file that gets copied into the source directory before
    builds. Later on these cmake changes will be integrated into ROCdbgapi
    and the local file will be removed.
    
    * Test Plan
    
    - Validate that ROCdbgapi builds and pulls its libbacktrace dependency.
    - Validate that ROCdbgapi's shared object exists and matches the name.
    
    * Test Result
    
    All tests passed.

--- build_tools/fetch_sources.py
+++ build_tools/fetch_sources.py
@@ -92,6 +92,8 @@ def get_enabled_projects(args) -> List[str]:
         projects.extend(args.system_projects)
     if args.include_compilers:
         projects.extend(args.compiler_projects)
+    if args.include_debug_tools:
+        projects.extend(args.debug_tools)
     if args.include_rocm_libraries:
         projects.extend(["rocm-libraries"])
     if args.include_rocm_systems:
@@ -424,6 +426,12 @@ def main(argv):
         help="Include compilers",
     )
     parser.add_argument(
+        "--include-debug-tools",
+        default=True,
+        action=argparse.BooleanOptionalAction,
+        help="Include ROCm debugging tools",
+    )
+    parser.add_argument(
         "--include-rocm-libraries",
         default=True,
         action=argparse.BooleanOptionalAction,
@@ -526,6 +534,14 @@ def main(argv):
                 "rocm-libraries",
             ]
         ),
+    )
+    parser.add_argument(
+        "--debug-tools",
+        nargs="+",
+        type=str,
+        default=[
+            "amd-dbgapi",
+        ],
     )
     args = parser.parse_args(argv)
 
