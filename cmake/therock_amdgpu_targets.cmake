aecd29a68d2bf685a389b139776b26ad6e4cd77d
geomin12 <geomin12@amd.com>
2025-11-18 14:24:35 -0800

    excluding rocwmma

--- cmake/therock_amdgpu_targets.cmake
+++ cmake/therock_amdgpu_targets.cmake
@@ -149,6 +149,7 @@ therock_add_amdgpu_target(gfx1152 "" FAMILY igpu-all gfx115X-all gfx115X-igpu
   EXCLUDE_TARGET_PROJECTS
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
     rccl  # https://github.com/ROCm/TheRock/issues/150
+    rocWMMA # https://github.com/ROCm/TheRock/issues/1944
 )
 
 # gfx120X family
