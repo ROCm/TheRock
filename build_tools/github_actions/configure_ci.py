10a1c80b5e6eec84c5cf7d38c72e1bcd71a829b4
geomin12 <geomin12@amd.com>
2025-11-18 17:43:16 -0800

    Adding fix

--- build_tools/github_actions/configure_ci.py
+++ build_tools/github_actions/configure_ci.py
@@ -394,7 +394,10 @@ def matrix_generator(
 
         # For nightly runs, we want to run full tests regardless of limited machines, so we delete the sanity_check_only_for_family option
         for key in lookup_matrix:
-            if "sanity_check_only_for_family" in lookup_matrix[key][platform]:
+            if (
+                platform in lookup_matrix[key]
+                and "sanity_check_only_for_family" in lookup_matrix[key][platform]
+            ):
                 del lookup_matrix[key][platform]["sanity_check_only_for_family"]
 
     # Ensure the lists are unique
