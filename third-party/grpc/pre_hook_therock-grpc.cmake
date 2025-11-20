8cdc2a5dc53582b71ddef439326eea6f47f79bac
Jenkins <jenkins-compute@amd.com>
2025-11-20 04:15:04 +0000

    Correct gRPC pre_hook variable scope and clean up RDC checks
    - Fix pre_hook_therock-grpc.cmake to use set()/string(APPEND)
    - Consolidate RDC platform check within single if(THEROCK_ENABLE_RDC) block
    - Remove redundant comments for cleaner code
    
    Signed-off-by: Jenkins <jenkins-compute@amd.com>

--- third-party/grpc/pre_hook_therock-grpc.cmake
+++ third-party/grpc/pre_hook_therock-grpc.cmake
@@ -2,25 +2,19 @@
 
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
   # C++ standard and PIC
-  list(APPEND CMAKE_ARGS
-    -DCMAKE_CXX_STANDARD=17
-    -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-  )
+  set(CMAKE_CXX_STANDARD 17)
+  set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
   # Hide symbols to prevent ODR violations
-  list(APPEND CMAKE_ARGS
-    -DCMAKE_CXX_VISIBILITY_PRESET=hidden
-    -DCMAKE_C_VISIBILITY_PRESET=hidden
-    -DCMAKE_VISIBILITY_INLINES_HIDDEN=ON
-  )
+  set(CMAKE_CXX_VISIBILITY_PRESET hidden)
+  set(CMAKE_C_VISIBILITY_PRESET hidden)
+  set(CMAKE_VISIBILITY_INLINES_HIDDEN ON)
 
   # Exclude static library symbols from exports
-  list(APPEND CMAKE_ARGS
-    "-DCMAKE_SHARED_LINKER_FLAGS=-Wl,--exclude-libs,ALL"
-    "-DCMAKE_EXE_LINKER_FLAGS=-Wl,--exclude-libs,ALL"
-    "-DCMAKE_MODULE_LINKER_FLAGS=-Wl,--exclude-libs,ALL"
-  )
+  string(APPEND CMAKE_SHARED_LINKER_FLAGS " -Wl,--exclude-libs,ALL")
+  string(APPEND CMAKE_EXE_LINKER_FLAGS " -Wl,--exclude-libs,ALL")
+  string(APPEND CMAKE_MODULE_LINKER_FLAGS " -Wl,--exclude-libs,ALL")
 elseif(CMAKE_SYSTEM_NAME STREQUAL "Windows")
-  # TODO: Add Windows-specific configuration
-  message(WARNING "gRPC on Windows is not yet supported; Windows-specific flags not set")
+  # TODO: Add Windows-specific configuration when gRPC is needed on Windows
+  message(STATUS "gRPC on Windows: configuration pending")
 endif()
