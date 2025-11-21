c8ed6845d871d0dff1a07876e83e7df40b24b18f
geomin12 <geomin12@amd.com>
2025-11-20 17:27:49 -0800

    Revert back to all

--- build_tools/github_actions/amdgpu_family_matrix.py
+++ build_tools/github_actions/amdgpu_family_matrix.py
@@ -58,7 +58,7 @@ amdgpu_family_info_matrix_presubmit = {
         },
         "windows": {
             "test-runs-on": "windows-gfx110X-gpu-rocm",
-            "family": "gfx110X-dgpu",
+            "family": "gfx110X-all",
             "bypass_tests_for_releases": True,
             "build_variants": ["release"],
             "sanity_check_only_for_family": True,
