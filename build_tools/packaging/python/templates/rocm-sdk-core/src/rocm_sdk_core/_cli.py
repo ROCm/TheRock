b5fd55878f6aeca10ed7cf8518a4c305e35304d0
Scott Todd <scott.todd0@gmail.com>
2025-12-04 15:56:18 -0800

    Redirect base exec path for CLI scripts if rocm[devel] is installed

--- build_tools/packaging/python/templates/rocm-sdk-core/src/rocm_sdk_core/_cli.py
+++ build_tools/packaging/python/templates/rocm-sdk-core/src/rocm_sdk_core/_cli.py
@@ -9,17 +9,34 @@ from pathlib import Path
 from ._dist_info import ALL_PACKAGES
 
 CORE_PACKAGE = ALL_PACKAGES["core"]
-PLATFORM_NAME = CORE_PACKAGE.get_py_package_name()
-PLATFORM_MODULE = importlib.import_module(PLATFORM_NAME)
-# NOTE: dependent on there being an __init__.py in the platform package.
-PLATFORM_PATH = Path(PLATFORM_MODULE.__file__).parent
+CORE_PY_PACKAGE_NAME = CORE_PACKAGE.get_py_package_name()
+# NOTE: dependent on there being an __init__.py in the core package.
+CORE_MODULE = importlib.import_module(CORE_PY_PACKAGE_NAME)
+CORE_MODULE_PATH = Path(CORE_MODULE.__file__).parent
+
+DEVEL_PACKAGE = ALL_PACKAGES["devel"]
+DEVEL_PY_PACKAGE_NAME = DEVEL_PACKAGE.get_py_package_name()
+DEVEL_DIST_PACKAGE_NAME = DEVEL_PACKAGE.get_dist_package_name()
+
+try:
+    # NOTE: dependent on there being an __init__.py in the devel package.
+    # The package must be initialized (e.g. with `rocm-sdk init`) first.
+    # TODO(#1880): auto-init if DEVEL_DIST_PACKAGE_NAME (rocm_sdk_devel) exists?
+    DEVEL_MODULE = importlib.import_module(DEVEL_PY_PACKAGE_NAME)
+    DEVEL_MODULE_PATH = Path(DEVEL_MODULE.__file__).parent
+
+    # Devel module is available, use it.
+    MODULE_PATH = DEVEL_MODULE_PATH
+except ModuleNotFoundError:
+    # Fallback to the core module.
+    MODULE_PATH = CORE_MODULE_PATH
 
 is_windows = platform.system() == "Windows"
 exe_suffix = ".exe" if is_windows else ""
 
 
 def _exec(relpath: str):
-    full_path = PLATFORM_PATH / (relpath + exe_suffix)
+    full_path = MODULE_PATH / (relpath + exe_suffix)
     os.execv(full_path, [str(full_path)] + sys.argv[1:])
 
 
