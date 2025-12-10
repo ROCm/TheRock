c6ebecd121c674de637a35a35e1436994db6aae3
jharryma <jharryma@amd.com>
2025-12-10 15:42:45 -0800

    address comments

--- build_tools/github_actions/test_executable_scripts/test_aqlprofile.py
+++ build_tools/github_actions/test_executable_scripts/test_aqlprofile.py
@@ -16,12 +16,8 @@ platform = os.getenv("RUNNER_OS", "linux").lower()
 
 # Sharding
 env = os.environ.copy()
-env["GTEST_SHARD_INDEX"] = str(int(os.getenv("SHARD_INDEX", "1")) - 1)
-env["GTEST_TOTAL_SHARDS"] = str(int(os.getenv("TOTAL_SHARDS", "1")))
-
-env["LD_LIBRARY_PATH"] = f"../../lib/"
-cmd = "run_tests.sh"
-cmd = f"{OUTPUT_ARTIFACTS_DIR}/share/hsa-amd-aqlprofile/run_tests.sh"
+env["LD_LIBRARY_PATH"] = ".." / ".." / "lib"
+cmd = {OUTPUT_ARTIFACTS_DIR} / "share" / "hsa-amd-aqlprofile" / "run_tests.sh"
 
 logging.info(f"++ Exec {cmd}")
 
