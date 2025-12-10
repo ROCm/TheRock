1eedbc4929a1ace3b2f4db880ee76256dc783007
jharryma <jharryma@amd.com>
2025-12-10 15:42:45 -0800

    address comments
    refactor test_awlprofile.py to use pathlib
    
    missing quotes

--- build_tools/github_actions/test_executable_scripts/test_aqlprofile.py
+++ build_tools/github_actions/test_executable_scripts/test_aqlprofile.py
@@ -9,19 +9,12 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 # repo + dirs
 SCRIPT_DIR = Path(__file__).resolve().parent
-THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
-OUTPUT_ARTIFACTS_DIR = os.getenv("OUTPUT_ARTIFACTS_DIR", "")
+THEROCK_DIR = Path(os.getenv("OUTPUT_ARTIFACTS_DIR")).resolve()
 env = os.environ.copy()
 platform = os.getenv("RUNNER_OS", "linux").lower()
 
-# Sharding
-env = os.environ.copy()
-env["GTEST_SHARD_INDEX"] = str(int(os.getenv("SHARD_INDEX", "1")) - 1)
-env["GTEST_TOTAL_SHARDS"] = str(int(os.getenv("TOTAL_SHARDS", "1")))
-
-env["LD_LIBRARY_PATH"] = f"../../lib/"
-cmd = "run_tests.sh"
-cmd = f"{OUTPUT_ARTIFACTS_DIR}/share/hsa-amd-aqlprofile/run_tests.sh"
+env["LD_LIBRARY_PATH"] = THEROCK_DIR / "lib"
+cmd = THEROCK_DIR / "share" / "hsa-amd-aqlprofile" / "run_tests.sh"
 
 logging.info(f"++ Exec {cmd}")
 
