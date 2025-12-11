fc93bab429b816c93db1fd0977c4743b80167011
jharryma <jharryma@amd.com>
2025-12-11 15:17:17 -0800

    refactor test_awlprofile.py to use pathlib

--- build_tools/github_actions/test_executable_scripts/test_aqlprofile.py
+++ build_tools/github_actions/test_executable_scripts/test_aqlprofile.py
@@ -9,15 +9,12 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 # repo + dirs
 SCRIPT_DIR = Path(__file__).resolve().parent
-THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
-OUTPUT_ARTIFACTS_DIR = os.getenv("OUTPUT_ARTIFACTS_DIR", "")
+THEROCK_DIR = Path(os.getenv("OUTPUT_ARTIFACTS_DIR")).resolve()
 env = os.environ.copy()
 platform = os.getenv("RUNNER_OS", "linux").lower()
 
-# Sharding
-env = os.environ.copy()
-env["LD_LIBRARY_PATH"] = ".." / ".." / "lib"
-cmd = {OUTPUT_ARTIFACTS_DIR} / "share" / "hsa-amd-aqlprofile" / "run_tests.sh"
+env["LD_LIBRARY_PATH"] = THEROCK_DIR / 'lib'
+cmd = THEROCK_DIR / "share" / "hsa-amd-aqlprofile" / "run_tests.sh"
 
 logging.info(f"++ Exec {cmd}")
 
