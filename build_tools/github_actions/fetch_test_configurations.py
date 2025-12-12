8812787bddd3cbcbc566fa65841241a95e71e00b
jharryma <jharryma@amd.com>
2025-12-11 22:25:01 -0800

    add aqlprofile to install_rocm_from_artifacts.py

--- build_tools/github_actions/fetch_test_configurations.py
+++ build_tools/github_actions/fetch_test_configurations.py
@@ -261,8 +261,8 @@ test_matrix = {
     },
     # aqlprofile tests
     "aqlprofile": {
-        "job_name": "apqlprofile",
-        "fetch_artifact_args": "--tests",
+        "job_name": "aplprofile",
+        "fetch_artifact_args": "--aqlprofile --tests",
         "timeout_minutes": 5,
         "test_script": f"python {_get_script_path('test_aqlprofile.py')}",
         "platform": ["linux"],
