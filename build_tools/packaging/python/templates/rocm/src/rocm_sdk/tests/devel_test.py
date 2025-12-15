a492f146d2a807259b12bcb7c9858d86d63733f4
Joseph Macaranas <Joseph.Macaranas@amd.com>
2025-12-15 14:54:27 -0500

    remove excess parentheses

--- build_tools/packaging/python/templates/rocm/src/rocm_sdk/tests/devel_test.py
+++ build_tools/packaging/python/templates/rocm/src/rocm_sdk/tests/devel_test.py
@@ -142,7 +142,7 @@ class ROCmDevelTest(unittest.TestCase):
                 # Internal rocprofiler-sdk libraries are meant to be pre-loaded
                 # explicitly and cannot necessarily be loaded standalone.
                 continue
-            if ("libtest_linking_lib" in str(so_path)):
+            if "libtest_linking_lib" in str(so_path):
                 # rocprim unit tests, not actual library files
                 continue
             with self.subTest(msg="Check shared library loads", so_path=so_path):
