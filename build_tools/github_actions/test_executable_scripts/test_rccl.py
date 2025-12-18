ddb6a49e3a1fa46ea4de22fa43e9aa91292c9789
aravind-ravi1206 <therockbot@amd.com>
2025-12-18 20:43:22 +0000

    Fixed typo

--- build_tools/github_actions/test_executable_scripts/test_rccl.py
+++ build_tools/github_actions/test_executable_scripts/test_rccl.py
@@ -17,7 +17,7 @@ class TestRCCL:
         environ_vars = os.environ.copy()
         environ_vars["UT_MIN_GPUS"] = "2"
         environ_vars["UT_MAX_GPUS"] = "2"
-        environ_vars["UT_POW2_GPUS=1"] = "1"
+        environ_vars["UT_POW2_GPUS"] = "1"
         environ_vars["UT_PROCESS_MASK"] = "1"
         cmd = [f"{THEROCK_BIN_DIR}/rccl-UnitTests"]
         logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmd)}")
