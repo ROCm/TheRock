490a4e3f4212414c712a8a037b4d5232c4293433
agunashe <ajay.gunashekar@amd.com>
2025-12-17 20:06:22 -0600

    fix more rock comments
    - created main and functions
    - move code to relatable functions
    - using rocm-systems users/agunashe/skip_rock_failures
    - install pkgconfiglite to test the Simde find

--- build_tools/github_actions/test_executable_scripts/test_hiptests.py
+++ build_tools/github_actions/test_executable_scripts/test_hiptests.py
@@ -17,15 +17,14 @@ if THEROCK_BIN_DIR_STR is None:
 THEROCK_BIN_DIR = Path(THEROCK_BIN_DIR_STR)
 SCRIPT_DIR = Path(__file__).resolve().parent
 THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
-
 SHARD_INDEX = os.getenv("SHARD_INDEX", 1)
 TOTAL_SHARDS = os.getenv("TOTAL_SHARDS", 1)
-CATCH_TESTS_PATH = f"{THEROCK_BIN_DIR}/../share/hip/catch_tests"
+CATCH_TESTS_PATH = str(Path(THEROCK_BIN_DIR).parent / "share" / "hip" / "catch_tests")
 if not os.path.isdir(CATCH_TESTS_PATH):
     logging.info(f"++ Error: catch tests not found in {CATCH_TESTS_PATH}")
     sys.exit(1)
-
 env = os.environ.copy()
+
 
 def get_test_count():
     cmd = ["ctest", "--show-only=json-v1"]
@@ -51,51 +50,56 @@ def get_test_range_per_shard(total_test_count: int, total_shards, shard_index):
         f"""++ hip-tests ctest: shard {shard_index} / {total_shards}. Running:{tests_per_shard} tests""")
     return [current_index, end_index]
 
+def copy_dlls_exe_path():
+    if platform.system() == "Windows":
+        # hip and comgr dlls need to be copied to the same folder as exectuable
+        dlls_pattern = ["amdhip64*.dll", "amd_comgr*.dll", "hiprtc*.dll"]
+        dlls_to_copy = []
+        for pattern in dlls_pattern:
+            dlls_to_copy.extend(f"{THEROCK_BIN_DIR}.glob(pattern)")
+        for dll in dlls_to_copy:
+            try:
+                shutil.copy(dll, CATCH_TESTS_PATH)
+                logging.info(f"++ Copied: {dll} to {CATCH_TESTS_PATH}")
+            except Exception as e:
+                logging.info(f"++ Error copying {dll}: {e}")
 
-if platform.system() == "Windows":
-    # hip and comgr dlls need to be copied to the same folder as exectuable
-    dlls_pattern = ["amdhip64*.dll", "amd_comgr*.dll", "hiprtc*.dll"]
-    dlls_to_copy = []
-    for pattern in dlls_pattern:
-        dlls_to_copy.extend(f"{THEROCK_BIN_DIR}.glob(pattern)")
-    for dll in dlls_to_copy:
-        try:
-            shutil.copy(dll, CATCH_TESTS_PATH)
-            logging.info(f"++ Copied: {dll} to {CATCH_TESTS_PATH}")
-        except Exception as e:
-            logging.info(f"++ Error copying {dll}: {e}")
-else:
-    HIP_LIB_PATH = f"{THEROCK_BIN_DIR}/../lib"
-    logging.info(f"++ Setting LD_LIBRARY_PATH={HIP_LIB_PATH}")
-    if "LD_LIBRARY_PATH" in env:
-        env["LD_LIBRARY_PATH"] = f"{HIP_LIB_PATH}:{env['LD_LIBRARY_PATH']}"
+def setup_env(env):
+    # catch/ctest framework
+    # Linux
+    #   LD_LIBRARY_PATH needs to be used
+    #   tests are hardcoded to look at THEROCK_BIN_DIR or /opt/rocm/lib path
+    # Windows
+    #   tests load the dlls present in the local exe folder
+    # Set ROCM Path, to find rocm_agent_enum etc
+    ROCM_PATH = f"{THEROCK_BIN_DIR}.resolve().parent"
+    env["ROCM_PATH"] = str(ROCM_PATH)
+    if platform.system() == "Linux":
+        HIP_LIB_PATH = Path(THEROCK_BIN_DIR).parent / "lib"
+        logging.info(f"++ Setting LD_LIBRARY_PATH={HIP_LIB_PATH}")
+        if "LD_LIBRARY_PATH" in env:
+            env["LD_LIBRARY_PATH"] = f"{HIP_LIB_PATH}:{env['LD_LIBRARY_PATH']}"
+        else:
+            env["LD_LIBRARY_PATH"] = HIP_LIB_PATH
     else:
-        env["LD_LIBRARY_PATH"] = HIP_LIB_PATH
+        copy_dlls_exe_path()
 
+def execute_tests(env):
+    total_tests = get_test_count()
+    index_start, index_end = get_test_range_per_shard(total_tests, int(TOTAL_SHARDS), int(SHARD_INDEX))
+    cmd = [
+        "ctest",
+        "-I",
+        f"{index_start},{index_end}",
+        "--test-dir",
+        CATCH_TESTS_PATH,
+        "--output-on-failure",
+        "--timeout",
+        "600"
+    ]
+    logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmd)}")
+    subprocess.run(cmd, cwd=THEROCK_DIR, check=True, env=env)
 
-# catch/ctest framework 
-# Linux
-#   LD_LIBRARY_PATH needs to be used
-#   tests are hardcoded to look at THEROCK_BIN_DIR or /opt/rocm/lib path
-# Windows
-#   tests load the dlls present in the local exe folder
-# Set ROCM Path, to find rocm_agent_enum etc
-ROCM_PATH = f"{THEROCK_BIN_DIR}.resolve().parent"
-env["ROCM_PATH"] = str(ROCM_PATH)
-
-
-total_tests = get_test_count()
-index_start, index_end = get_test_range_per_shard(total_tests, int(TOTAL_SHARDS), int(SHARD_INDEX))
-cmd = [
-    "ctest",
-    "-I",
-    f"{index_start},{index_end}",
-    "--test-dir",
-    CATCH_TESTS_PATH,
-    "--output-on-failure",
-    "--timeout",
-    "600"
-]
-
-logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmd)}")
-subprocess.run(cmd, cwd=THEROCK_DIR, check=True, env=env)
+if __name__ == "__main__":
+    setup_env(env)
+    execute_tests(env)
