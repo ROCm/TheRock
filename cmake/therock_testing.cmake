f8244441c83be352824465c3db1f8f0582897c21
geomin12 <geomin12@amd.com>
2025-12-19 09:58:16 -0800

    adding bin

--- cmake/therock_testing.cmake
+++ cmake/therock_testing.cmake
@@ -20,11 +20,8 @@ function(therock_test_validate_shared_lib)
 
   separate_arguments(CMAKE_C_COMPILER_LIST UNIX_COMMAND "${CMAKE_C_COMPILER}")
 
-  message(STATUS "CMAKE_SOURCE_DIR = ${CMAKE_SOURCE_DIR}")
-  message(STATUS "Expected clang path = ${CMAKE_SOURCE_DIR}/lib/llvm/bin/clang")
-
   set(CLANG_EXECUTABLE
-    "${CMAKE_SOURCE_DIR}/lib/llvm/bin/clang"
+    "${CMAKE_SOURCE_DIR}/build/lib/llvm/bin/clang"
     CACHE FILEPATH "Clang executable"
   )
 
