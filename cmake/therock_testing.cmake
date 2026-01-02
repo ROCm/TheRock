5db4cfefb14e6cdfa9fdfeec83469aa3fd443554
geomin12 <geomin12@amd.com>
2026-01-02 12:11:28 -0800

    skip validation

--- cmake/therock_testing.cmake
+++ cmake/therock_testing.cmake
@@ -8,6 +8,12 @@ function(therock_test_validate_shared_lib)
     "PATH"
     "LIB_NAMES"
   )
+
+  # Skip shared-library dlopen validation for ASAN builds
+  if(THEROCK_SANITIZER STREQUAL "ASAN")
+    return()
+  endif()
+
   if(WIN32)
     # This helper is Linux only. In the future, we can have separate DLL_NAMES
     # and verify.
@@ -18,34 +24,12 @@ function(therock_test_validate_shared_lib)
     cmake_path(ABSOLUTE_PATH ARG_PATH BASE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")
   endif()
 
-  separate_arguments(CMAKE_C_COMPILER_LIST UNIX_COMMAND "${CMAKE_C_COMPILER}")
-
-  set(CLANG_EXECUTABLE
-    "${CMAKE_SOURCE_DIR}/build/lib/llvm/bin/clang"
-    CACHE FILEPATH "Clang executable"
-  )
-
-  execute_process(
-    COMMAND ${CLANG_EXECUTABLE} --print-runtime-dir
-    OUTPUT_VARIABLE CLANG_RUNTIME_DIR
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
-
-  if(NOT CLANG_RUNTIME_DIR)
-    message("clang --print-runtime-dir returned empty: ${CMAKE_CURRENT_BINARY_DIR}")
-  endif()
-
-  set(ASAN_RUNTIME "${CLANG_RUNTIME_DIR}/libclang_rt.asan-x86_64.so")
-
   foreach(lib_name ${ARG_LIB_NAMES})
     add_test(
       NAME therock-validate-shared-lib-${lib_name}
       COMMAND
-        env
-        LD_PRELOAD=${ASAN_RUNTIME}
-        LD_LIBRARY_PATH=${CLANG_RUNTIME_DIR}:$ENV{LD_LIBRARY_PATH}
-          "${Python3_EXECUTABLE}" "${THEROCK_SOURCE_DIR}/build_tools/validate_shared_library.py"
-          "${ARG_PATH}/${lib_name}"
+        "${Python3_EXECUTABLE}" "${THEROCK_SOURCE_DIR}/build_tools/validate_shared_library.py"
+        "${ARG_PATH}/${lib_name}"
     )
   endforeach()
 endfunction()
