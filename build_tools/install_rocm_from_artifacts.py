f28d32eeec1f41e53c6403c8ae18c061adca5a5e
Michael Obersteiner <Michael.Obersteiner@amd.com>
2026-01-07 09:07:07 +0000

    fix: sort args

--- build_tools/install_rocm_from_artifacts.py
+++ build_tools/install_rocm_from_artifacts.py
@@ -245,8 +245,8 @@ def retrieve_artifacts_by_run_id(args):
         if args.libhipcxx:
             extra_artifacts.append("libhipcxx")
             argv.append("amd-llvm_dev")
-            argv.append("base_dev_generic")
             argv.append("amd-llvm_lib")
+            argv.append("base_dev_generic")
 
         extra_artifact_patterns = [f"{a}_lib" for a in extra_artifacts]
         if args.tests:
