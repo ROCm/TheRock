052a29c550b8576ceec8a827dab8e5753c8c3864
jbonnell-amd <jason.bonnell@amd.com>
2026-01-09 16:10:43 -0500

    Add back Cmake commands to build sdk tests

--- build_tools/github_actions/test_executable_scripts/test_rocprofiler_sdk.py
+++ build_tools/github_actions/test_executable_scripts/test_rocprofiler_sdk.py
@@ -14,11 +14,36 @@ logging.basicConfig(level=logging.INFO)
 # Env setup
 os.environ["HIP_PLATFORM"] = "amd"
 
+# CMake Init
+cmake_init_cmd = [
+    "cmake",
+    "-B",
+    "/tmp/rocprofiler-sdk-build-tests",
+    f"-DCMAKE_PREFIX_PATH={OUTPUT_ARTIFACTS_DIR}",
+    f"{OUTPUT_ARTIFACTS_DIR}/share/rocprofiler-sdk/tests",
+]
+
+logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmake_init_cmd)}")
+subprocess.run(cmake_init_cmd, cwd=THEROCK_DIR, check=True)
+
+# Cmake Build
+cmake_cmd = [
+    "cmake",
+    "--build",
+    "/tmp/rocprofiler-sdk-build-tests",
+    "--target",
+    "all",
+    "--parallel",
+    "8",
+]
+logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmake_cmd)}")
+subprocess.run(cmake_cmd, cwd=THEROCK_DIR, check=True)
+
 # Run tests
 cmd = [
     "ctest",
     "--test-dir",
-    f"{THEROCK_BIN_DIR}/rocprofiler-sdk",
+    f"{OUTPUT_ARTIFACTS_DIR}/share/rocprofiler-sdk/tests",
     "--output-on-failure",
     "--parallel",
     "8",
