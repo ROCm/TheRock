0ea61a03b20759e149c2231085f5350f45f0b781
jbonnell-amd <jason.bonnell@amd.com>
2026-01-13 16:40:09 -0500

    Add PATH to test_rocprofiler_compute.py to try and address rocminfo error

--- build_tools/github_actions/test_executable_scripts/test_rocprofiler_compute.py
+++ build_tools/github_actions/test_executable_scripts/test_rocprofiler_compute.py
@@ -14,12 +14,18 @@ environ_vars = os.environ.copy()
 environ_vars["ROCM_PATH"] = str(ROCM_PATH)
 
 old_pythonpath = os.getenv("PYTHONPATH", "")
+old_path = os.getenv("PATH", "")
 test_dir = f"{OUTPUT_ARTIFACTS_DIR}/libexec/rocprofiler-compute/tests"
 
 if old_pythonpath:
     os.environ["PYTHONPATH"] = f"{test_dir}:{old_pythonpath}"
 else:
     os.environ["PYTHONPATH"] = test_dir
+
+if old_path:
+    os.environ["PATH"] = f"{THEROCK_BIN_DIR}:{old_path}"
+else:
+    os.environ["PATH"] = THEROCK_BIN_DIR
 
 # Run tests
 cmd = [
