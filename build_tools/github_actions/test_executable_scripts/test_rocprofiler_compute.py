3fb442bda69e742186a3d9c4c19f933e2317c55b
jbonnell-amd <jason.bonnell@amd.com>
2026-01-14 11:52:53 -0500

    Change cwd, add LB_LIBRARY_PATH to test_rocprofiler_compute.py

--- build_tools/github_actions/test_executable_scripts/test_rocprofiler_compute.py
+++ build_tools/github_actions/test_executable_scripts/test_rocprofiler_compute.py
@@ -13,19 +13,29 @@ ROCM_PATH = Path(THEROCK_BIN_DIR).resolve().parent
 environ_vars = os.environ.copy()
 environ_vars["ROCM_PATH"] = str(ROCM_PATH)
 
+# Set up PYTHONPATH
 old_pythonpath = os.getenv("PYTHONPATH", "")
-old_path = os.getenv("PATH", "")
 test_dir = f"{OUTPUT_ARTIFACTS_DIR}/libexec/rocprofiler-compute/tests"
-
 if old_pythonpath:
     os.environ["PYTHONPATH"] = f"{test_dir}:{old_pythonpath}"
 else:
     os.environ["PYTHONPATH"] = test_dir
 
+# Set up PATH
+old_path = os.getenv("PATH", "")
 if old_path:
     os.environ["PATH"] = f"{THEROCK_BIN_DIR}:{old_path}"
 else:
     os.environ["PATH"] = THEROCK_BIN_DIR
+
+# Set up LD_LIBRARY_PATH
+old_ld_lib_path = os.getenv("LD_LIBRARY_PATH", "")
+if old_ld_lib_path:
+    os.environ["LD_LIBRARY_PATH"] = (
+        f"{OUTPUT_ARTIFACTS_DIR}/lib:{OUTPUT_ARTIFACTS_DIR}/lib/rocm_sysdeps/lib:{old_ld_lib_path}"
+    )
+else:
+    os.environ["LD_LIBRARY_PATH"] = f"{OUTPUT_ARTIFACTS_DIR}/lib"
 
 # Run tests
 cmd = [
@@ -38,6 +48,12 @@ cmd = [
     "--timeout",
     "1800",
 ]
-logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmd)}")
-
-subprocess.run(cmd, cwd=THEROCK_DIR, check=True, env=environ_vars)
+logging.info(
+    f"++ Exec [{OUTPUT_ARTIFACTS_DIR}/libexec/rocprofiler-compute]$ {shlex.join(cmd)}"
+)
+subprocess.run(
+    cmd,
+    cwd=f"{OUTPUT_ARTIFACTS_DIR}/libexec/rocprofiler-compute",
+    check=True,
+    env=environ_vars,
+)
