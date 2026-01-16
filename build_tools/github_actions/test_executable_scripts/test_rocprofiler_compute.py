d988e3769af1d9a01b3c725381080c8c98d46646
jbonnell-amd <jason.bonnell@amd.com>
2026-01-16 17:06:34 -0500

    Refactor environment setup, paths

--- build_tools/github_actions/test_executable_scripts/test_rocprofiler_compute.py
+++ build_tools/github_actions/test_executable_scripts/test_rocprofiler_compute.py
@@ -2,6 +2,7 @@ import logging
 import os
 import shlex
 import subprocess
+import sys
 from pathlib import Path
 
 THEROCK_BIN_DIR = os.getenv("THEROCK_BIN_DIR")
@@ -9,25 +10,31 @@ OUTPUT_ARTIFACTS_DIR = os.getenv("OUTPUT_ARTIFACTS_DIR")
 SCRIPT_DIR = Path(__file__).resolve().parent
 THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
 
-# Set up ROCm path
-ROCM_PATH = Path(THEROCK_BIN_DIR).resolve().parent
 environ_vars = os.environ.copy()
-environ_vars["ROCM_PATH"] = str(ROCM_PATH)
 
 # Resolve absolute paths
-OUTPUT_ARTIFACTS_PATH = Path(OUTPUT_ARTIFACTS_DIR).resolve()
 THEROCK_BIN_PATH = Path(THEROCK_BIN_DIR).resolve()
-print(f"OUTPUT_ARTIFACTS_PATH: {OUTPUT_ARTIFACTS_DIR}")
+THEROCK_PATH = THEROCK_BIN_PATH.parent
+THEROCK_LIB_PATH = str(THEROCK_PATH / "lib")
+
+print(f"THEROCK_PATH: {THEROCK_PATH}")
 print(f"THEROCK_BIN_PATH: {THEROCK_BIN_PATH}")
+print(f"THEROCK_LIB_PATH: {THEROCK_PATH}")
+
+# Set up ROCM_PATH
+environ_vars["ROCM_PATH"] = str(THEROCK_PATH)
 
 # Set up PYTHONPATH
 old_pythonpath = os.getenv("PYTHONPATH", "")
-module_dir = f"{THEROCK_BIN_PATH}/../libexec/rocprofiler-compute/tests"
+module_dir = f"{THEROCK_DIR}/libexec/rocprofiler-compute/tests"
 print(f"Module Directory: {module_dir}")
 if old_pythonpath:
     os.environ["PYTHONPATH"] = f"{module_dir}:{old_pythonpath}"
 else:
     os.environ["PYTHONPATH"] = module_dir
+
+# See if this helps pick up modules properly
+sys.path.append(os.path.dirname(__file__))
 
 # Set up PATH
 old_path = os.getenv("PATH", "")
@@ -40,34 +47,32 @@ else:
 
 # Set up LD_LIBRARY_PATH
 old_ld_lib_path = os.getenv("LD_LIBRARY_PATH", "")
-rocm_lib = str(OUTPUT_ARTIFACTS_PATH / "lib")
-print(f"ROCm Lib Directory: {rocm_lib}")
 if old_ld_lib_path:
     os.environ["LD_LIBRARY_PATH"] = (
-        f"{rocm_lib}:{rocm_lib}/rocm_sysdeps/lib:{old_ld_lib_path}"
+        f"{THEROCK_LIB_PATH}:{THEROCK_LIB_PATH}/rocm_sysdeps/lib:{old_ld_lib_path}"
     )
 else:
-    os.environ["LD_LIBRARY_PATH"] = rocm_lib
+    os.environ["LD_LIBRARY_PATH"] = THEROCK_LIB_PATH
 
 # Print out all env vars
-for key, value in os.environ.items():
+for key, value in environ_vars.items():
     print(f"{key}: {value}")
 
 # Run tests
 cmd = [
     "ctest",
+    "--test-dir",
+    f"{THEROCK_PATH}/libexec/rocprofiler-compute",
     "--output-on-failure",
     "--parallel",
     "8",
     "--timeout",
     "1800",
 ]
-logging.info(
-    f"++ Exec [{OUTPUT_ARTIFACTS_DIR}/libexec/rocprofiler-compute]$ {shlex.join(cmd)}"
-)
+logging.info(f"++ Exec [{THEROCK_PATH}]$ {shlex.join(cmd)}")
 subprocess.run(
     cmd,
-    cwd=f"{OUTPUT_ARTIFACTS_DIR}/libexec/rocprofiler-compute",
+    cwd=THEROCK_PATH,
     check=True,
     env=environ_vars,
 )
