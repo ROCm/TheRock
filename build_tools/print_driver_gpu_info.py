b910f0b3d0150f744de7268200510a3894c540d8
geomin12 <geomin12@amd.com>
2026-01-16 15:00:02 -0800

    Adding correct arch

--- build_tools/print_driver_gpu_info.py
+++ build_tools/print_driver_gpu_info.py
@@ -22,8 +22,8 @@ import sys
 from typing import List, Optional
 
 AMDGPU_FAMILIES = os.getenv("AMDGPU_FAMILIES")
-# TODO(#2964): Remove gfx950 once amdsmi static does not timeout
-unsupported_amdsmi_families = ["gfx1151", "gfx950"]
+# TODO(#2964): Remove gfx950-dcgpu once amdsmi static does not timeout
+unsupported_amdsmi_families = ["gfx1151", "gfx950-dcgpu"]
 
 
 def log(*args, **kwargs):
