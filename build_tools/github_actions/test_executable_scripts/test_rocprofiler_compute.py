df38263c8251b0d78f041a75deeb78157e8a3862
jbonnell-amd <jason.bonnell@amd.com>
2026-01-19 14:49:02 -0500

    Update environ_vars in test_rocprofiler_compute.py

--- build_tools/github_actions/test_executable_scripts/test_rocprofiler_compute.py
+++ build_tools/github_actions/test_executable_scripts/test_rocprofiler_compute.py
@@ -2,15 +2,12 @@ import logging
 import os
 import shlex
 import subprocess
-import sys
 from pathlib import Path
 
 THEROCK_BIN_DIR = os.getenv("THEROCK_BIN_DIR")
 OUTPUT_ARTIFACTS_DIR = os.getenv("OUTPUT_ARTIFACTS_DIR")
 SCRIPT_DIR = Path(__file__).resolve().parent
 THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
-
-environ_vars = os.environ.copy()
 
 # Resolve absolute paths
 THEROCK_BIN_PATH = Path(THEROCK_BIN_DIR).resolve()
@@ -22,37 +19,36 @@ print(f"THEROCK_BIN_PATH: {THEROCK_BIN_PATH}")
 print(f"THEROCK_LIB_PATH: {THEROCK_PATH}")
 
 # Set up ROCM_PATH
+environ_vars = os.environ.copy()
 environ_vars["ROCM_PATH"] = str(THEROCK_PATH)
 
-# Set up PYTHONPATH
+# Set up PYTHONPATH (for test_utils.py)
 old_pythonpath = os.getenv("PYTHONPATH", "")
 module_dir = f"{THEROCK_PATH}/libexec/rocprofiler-compute/tests"
 print(f"Module Directory: {module_dir}")
 if old_pythonpath:
-    os.environ["PYTHONPATH"] = f"{module_dir}:{old_pythonpath}"
+    environ_vars["PYTHONPATH"] = f"{module_dir}:{old_pythonpath}"
 else:
-    os.environ["PYTHONPATH"] = module_dir
-
-# See if this helps pick up modules properly
-sys.path.append(os.path.dirname(__file__))
+    environ_vars["PYTHONPATH"] = module_dir
 
 # Set up PATH
 old_path = os.getenv("PATH", "")
 rocm_bin = str(THEROCK_BIN_PATH)
 print(f"ROCm Bin Directory: {rocm_bin}")
 if old_path:
-    os.environ["PATH"] = f"{rocm_bin}:{old_path}"
+    environ_vars["PATH"] = f"{rocm_bin}:{old_path}"
 else:
-    os.environ["PATH"] = rocm_bin
+    environ_vars["PATH"] = rocm_bin
 
 # Set up LD_LIBRARY_PATH
 old_ld_lib_path = os.getenv("LD_LIBRARY_PATH", "")
+sysdeps_path = f"{THEROCK_LIB_PATH}/rocm_sysdeps/lib"
 if old_ld_lib_path:
-    os.environ["LD_LIBRARY_PATH"] = (
-        f"{THEROCK_LIB_PATH}:{THEROCK_LIB_PATH}/rocm_sysdeps/lib:{old_ld_lib_path}"
+    environ_vars["LD_LIBRARY_PATH"] = (
+        f"{THEROCK_LIB_PATH}:{sysdeps_path}:{old_ld_lib_path}"
     )
 else:
-    os.environ["LD_LIBRARY_PATH"] = THEROCK_LIB_PATH
+    environ_vars["LD_LIBRARY_PATH"] = f"{THEROCK_LIB_PATH}:{sysdeps_path}"
 
 # Print out all env vars
 for key, value in environ_vars.items():
