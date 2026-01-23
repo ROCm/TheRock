6082b1d917f2f286bbaf21901815dd239e80ce78
jbonnell-amd <jason.bonnell@amd.com>
2026-01-23 12:42:11 -0500

    Remove --parallel 8 option from ctest to avoid profile errors

--- build_tools/github_actions/test_executable_scripts/test_rocprofiler_compute.py
+++ build_tools/github_actions/test_executable_scripts/test_rocprofiler_compute.py
@@ -54,8 +54,6 @@ cmd = [
     "--test-dir",
     f"{THEROCK_PATH}/libexec/rocprofiler-compute",
     "--output-on-failure",
-    "--parallel",
-    "8",
     "--verbose",
     "-E",
     f"{EXCLUDED_TESTS}",
