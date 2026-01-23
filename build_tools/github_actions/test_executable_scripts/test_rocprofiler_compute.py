51c392ea66c7beea9eded65a91640c9f94c480f8
jbonnell-amd <jason.bonnell@amd.com>
2026-01-23 16:29:54 -0500

    Minor code cleanup on test_rocprofiler_compute.py

--- build_tools/github_actions/test_executable_scripts/test_rocprofiler_compute.py
+++ build_tools/github_actions/test_executable_scripts/test_rocprofiler_compute.py
@@ -4,27 +4,24 @@ import shlex
 import subprocess
 from pathlib import Path
 
+# Resolve paths
 THEROCK_BIN_DIR = os.getenv("THEROCK_BIN_DIR")
 OUTPUT_ARTIFACTS_DIR = os.getenv("OUTPUT_ARTIFACTS_DIR")
 SCRIPT_DIR = Path(__file__).resolve().parent
 THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
 
-# Resolve absolute paths
 THEROCK_BIN_PATH = Path(THEROCK_BIN_DIR).resolve()
 THEROCK_PATH = THEROCK_BIN_PATH.parent
 THEROCK_LIB_PATH = str(THEROCK_PATH / "lib")
-
-# Set up excluded tests (include Jiras)
-# AIPROFSDK-36: rocr issue causing test to fail
-EXCLUDED_TESTS = "test_profile_pc_sampling"
+ROCPROFILER_COMPUTE_DIRECTORY = f"{THEROCK_PATH}/libexec/rocprofiler-compute"
 
 # Set up ROCM_PATH
 environ_vars = os.environ.copy()
 environ_vars["ROCM_PATH"] = str(THEROCK_PATH)
 
-# Set up PYTHONPATH (for test_utils.py)
+# Set up PYTHONPATH
 old_pythonpath = os.getenv("PYTHONPATH", "")
-module_dir = f"{THEROCK_PATH}/libexec/rocprofiler-compute/tests"
+module_dir = f"{ROCPROFILER_COMPUTE_DIRECTORY}/tests"
 if old_pythonpath:
     environ_vars["PYTHONPATH"] = f"{module_dir}:{old_pythonpath}"
 else:
@@ -48,11 +45,15 @@ if old_ld_lib_path:
 else:
     environ_vars["LD_LIBRARY_PATH"] = f"{THEROCK_LIB_PATH}:{sysdeps_path}"
 
+# Set up excluded tests (include Jiras)
+# AIPROFSDK-36: rocr issue causing test to fail
+EXCLUDED_TESTS = "test_profile_pc_sampling"
+
 # Run tests
 cmd = [
     "ctest",
     "--test-dir",
-    f"{THEROCK_PATH}/libexec/rocprofiler-compute",
+    f"{ROCPROFILER_COMPUTE_DIRECTORY}",
     "--output-on-failure",
     "--verbose",
     "-E",
