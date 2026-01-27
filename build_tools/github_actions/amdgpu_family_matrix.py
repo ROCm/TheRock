feb11a572312c246ae780d9676d4d997282f1b52
geomin12 <geomin12@amd.com>
2026-01-27 02:49:21 -0800

    adding official label

--- build_tools/github_actions/amdgpu_family_matrix.py
+++ build_tools/github_actions/amdgpu_family_matrix.py
@@ -48,10 +48,10 @@ all_build_variants = {
 amdgpu_family_info_matrix_presubmit = {
     "gfx94x": {
         "linux": {
-            "test-runs-on": "linux-mi325-1gpu-ossci-rocm.test",
-            "test-runs-on-multi-gpu": "linux-mi325-8gpu-ossci-rocm.test",
+            "test-runs-on": "linux-mi325-1gpu-ossci-rocm",
+            "test-runs-on-multi-gpu": "linux-mi325-8gpu-ossci-rocm",
             # TODO(#2754): Add new benchmark-runs-on runner for benchmarks
-            "benchmark-runs-on": "linux-mi325-1gpu-ossci-rocm.test",
+            "benchmark-runs-on": "linux-mi325-8gpu-ossci-rocm",
             "family": "gfx94X-dcgpu",
             "build_variants": ["release", "asan"],
         }
