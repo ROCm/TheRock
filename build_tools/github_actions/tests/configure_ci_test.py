ff75e836e69dc950f4158693c970e2f8a83bf1f2
Laura Promberger <laura.promberger@amd.com>
2026-02-04 19:36:29 +0000

    move ci path filters out of configure_ci.py to ci_path_filters.py, rename better names for some functions.

--- build_tools/github_actions/tests/configure_ci_test.py
+++ build_tools/github_actions/tests/configure_ci_test.py
@@ -8,6 +8,7 @@ from unittest.mock import patch
 
 sys.path.insert(0, os.fspath(Path(__file__).parent.parent))
 import configure_ci
+from ci_path_filters import is_ci_run_required
 from benchmarks.benchmark_test_matrix import benchmark_matrix
 
 therock_test_runner_dict = {
@@ -57,53 +58,53 @@ class ConfigureCITest(unittest.TestCase):
             )
 
     ###########################################################################
-    # Tests for should_ci_run_given_modified_paths
+    # Tests for is_ci_run_required
 
     def test_run_ci_if_source_file_edited(self):
         paths = ["source_file.h"]
-        run_ci = configure_ci.should_ci_run_given_modified_paths(paths)
+        run_ci = is_ci_run_required(paths)
         self.assertTrue(run_ci)
 
     def test_dont_run_ci_if_only_markdown_files_edited(self):
         paths = ["README.md", "build_tools/README.md"]
-        run_ci = configure_ci.should_ci_run_given_modified_paths(paths)
+        run_ci = is_ci_run_required(paths)
         self.assertFalse(run_ci)
 
     def test_dont_run_ci_if_only_external_builds_edited(self):
         paths = ["external-builds/pytorch/CMakeLists.txt"]
-        run_ci = configure_ci.should_ci_run_given_modified_paths(paths)
+        run_ci = is_ci_run_required(paths)
         self.assertFalse(run_ci)
 
     def test_dont_run_ci_if_only_external_builds_edited(self):
         paths = ["experimental/file.h"]
-        run_ci = configure_ci.should_ci_run_given_modified_paths(paths)
+        run_ci = is_ci_run_required(paths)
         self.assertFalse(run_ci)
 
     def test_run_ci_if_related_workflow_file_edited(self):
         paths = [".github/workflows/ci.yml"]
-        run_ci = configure_ci.should_ci_run_given_modified_paths(paths)
+        run_ci = is_ci_run_required(paths)
         self.assertTrue(run_ci)
 
         paths = [".github/workflows/build_portable_linux_artifacts.yml"]
-        run_ci = configure_ci.should_ci_run_given_modified_paths(paths)
+        run_ci = is_ci_run_required(paths)
         self.assertTrue(run_ci)
 
         paths = [".github/workflows/build_artifact.yml"]
-        run_ci = configure_ci.should_ci_run_given_modified_paths(paths)
+        run_ci = is_ci_run_required(paths)
         self.assertTrue(run_ci)
 
     def test_dont_run_ci_if_unrelated_workflow_file_edited(self):
         paths = [".github/workflows/pre-commit.yml"]
-        run_ci = configure_ci.should_ci_run_given_modified_paths(paths)
+        run_ci = is_ci_run_required(paths)
         self.assertFalse(run_ci)
 
         paths = [".github/workflows/test_jax_dockerfile.yml"]
-        run_ci = configure_ci.should_ci_run_given_modified_paths(paths)
+        run_ci = is_ci_run_required(paths)
         self.assertFalse(run_ci)
 
     def test_run_ci_if_source_file_and_unrelated_workflow_file_edited(self):
         paths = ["source_file.h", ".github/workflows/pre-commit.yml"]
-        run_ci = configure_ci.should_ci_run_given_modified_paths(paths)
+        run_ci = is_ci_run_required(paths)
         self.assertTrue(run_ci)
 
     ###########################################################################
