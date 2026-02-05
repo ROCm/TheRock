6382c66498b9dab5a986ecc785dc53c5f7813bec
Bain Syrowik <bsyrowik@amd.com>
2026-02-05 14:15:35 -0800

    Try to trigger tests

--- build_tools/github_actions/amdgpu_family_matrix.py
+++ build_tools/github_actions/amdgpu_family_matrix.py
@@ -68,9 +68,7 @@ amdgpu_family_info_matrix_presubmit = {
             # Label is "linux-gfx110X-gpu-rocm"
             "test-runs-on": "linux-gfx1103-gpu-rocm",
             "family": "gfx110X-all",
-            "bypass_tests_for_releases": True,
             "build_variants": ["release"],
-            "sanity_check_only_for_family": True,
         },
         "windows": {
             "test-runs-on": "windows-gfx110X-gpu-rocm",
