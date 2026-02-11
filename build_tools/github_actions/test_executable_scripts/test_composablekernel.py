b6c1d5db942764bdd3959729d43bceb56cff8d52
Brock Hargreaves <brock.hargreaves@amd.com>
2026-02-11 18:17:51 -0500

    Add missing test file.

---
+++ build_tools/github_actions/test_executable_scripts/test_composablekernel.py
@@ -0,0 +1,24 @@
+import logging
+import os
+import shlex
+import subprocess
+from pathlib import Path
+
+THEROCK_BIN_DIR = os.getenv("THEROCK_BIN_DIR")
+SCRIPT_DIR = Path(__file__).resolve().parent
+THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
+AMDGPU_FAMILIES = os.getenv("AMDGPU_FAMILIES")
+
+logging.basicConfig(level=logging.INFO)
+
+cmd = [
+    "test_ck_tile_pooling",
+]
+
+logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmd)}")
+
+subprocess.run(
+    cmd,
+    cwd=THEROCK_DIR,
+    check=True,
+)
