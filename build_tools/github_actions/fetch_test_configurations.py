0107bc3e2bc3839a0ba6aa90010c65bc8009070f
Brock Hargreaves <brock.hargreaves@amd.com>
2026-02-11 17:36:48 -0500

    Go down to 1 shard for now.

--- build_tools/github_actions/fetch_test_configurations.py
+++ build_tools/github_actions/fetch_test_configurations.py
@@ -261,7 +261,7 @@ test_matrix = {
         "timeout_minutes": 60,
         "test_script": f"python {_get_script_path('test_composablekernel.py')}",
         "platform": ["linux", "windows"],
-        "total_shards": 4,
+        "total_shards": 1,
     },
     # TODO(iree-org/fusilli/issues/57): Enable fusilli tests once build is
     # enabled by default.
