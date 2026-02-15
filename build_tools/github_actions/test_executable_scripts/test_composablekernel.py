9ed1b67e8ea92f63a3fa65976a3ad9cbfe89aa5c
Brock Hargreaves <brock.hargreaves@amd.com>
2026-02-14 20:19:08 -0500

    Fix test path.

--- build_tools/github_actions/test_executable_scripts/test_composablekernel.py
+++ build_tools/github_actions/test_executable_scripts/test_composablekernel.py
@@ -8,17 +8,10 @@ THEROCK_BIN_DIR = os.getenv("THEROCK_BIN_DIR")
 SCRIPT_DIR = Path(__file__).resolve().parent
 THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
 AMDGPU_FAMILIES = os.getenv("AMDGPU_FAMILIES")
+environ_vars = os.environ.copy()
 
 logging.basicConfig(level=logging.INFO)
 
-cmd = [
-    "test_ck_tile_pooling",
-]
-
+cmd = [f"{THEROCK_BIN_DIR}/test_ck_tile_pooling"]
 logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmd)}")
-
-subprocess.run(
-    cmd,
-    cwd=THEROCK_DIR,
-    check=True,
-)
+subprocess.run(cmd, cwd=THEROCK_DIR, check=True, env=environ_vars)
