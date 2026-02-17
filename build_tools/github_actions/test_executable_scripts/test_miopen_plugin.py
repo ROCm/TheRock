d6cbd07d8d53a4847ee9f373746cffdae2feacf3
Adam Dickin <adam.dickin@amd.com>
2026-02-17 12:04:51 -0700

    Change ctest option from --verbose to --output-on-failure

--- build_tools/github_actions/test_executable_scripts/test_miopen_plugin.py
+++ build_tools/github_actions/test_executable_scripts/test_miopen_plugin.py
@@ -14,7 +14,7 @@ cmd = [
     "ctest",
     "--test-dir",
     f"{THEROCK_BIN_DIR}/miopen_legacy_plugin",
-    "--verbose",
+    "--output-on-failure",
     "--parallel",
     "8",
     "--timeout",
