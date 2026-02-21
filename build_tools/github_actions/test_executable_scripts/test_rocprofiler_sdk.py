fdc4149c4e94c987df112af94a0d5e5604b97bfc
Venkateshwar Reddy Kandula <vkandula@amd.com>
2026-02-21 02:04:55 -0600

    enable attach tests to check ptrace enabling in docker container works passes this tests

--- build_tools/github_actions/test_executable_scripts/test_rocprofiler_sdk.py
+++ build_tools/github_actions/test_executable_scripts/test_rocprofiler_sdk.py
@@ -80,10 +80,10 @@ subprocess.run(
     env=environ_vars,
 )
 
-CTEST_EXCLUDE = (
-    r"(tests\.integration\.execute\.rocprofv3-test-attachment-attach-once|"
-    r"tests\.integration\.validate\.rocprofv3-test-attachment-attach-once)"
-)
+# CTEST_EXCLUDE = (
+#     r"(tests\.integration\.execute\.rocprofv3-test-attachment-attach-once|"
+#     r"tests\.integration\.validate\.rocprofv3-test-attachment-attach-once)"
+# )
 
 # CTest
 ctest_cmd = [
@@ -91,8 +91,8 @@ ctest_cmd = [
     "--test-dir",
     "build",
     "--output-on-failure",
-    "-E",
-    CTEST_EXCLUDE,
+    # "-E",
+    # CTEST_EXCLUDE,
 ]
 
 logging.info(f"++ Exec [{ROCPROFILER_SDK_TESTS_DIRECTORY}]$ {shlex.join(ctest_cmd)}")
