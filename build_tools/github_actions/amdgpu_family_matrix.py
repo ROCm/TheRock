26b295dd73253b0de3908b7fbdaad9f8bd45ae7b
geomin12 <geomin12@amd.com>
2026-02-27 09:00:04 -0800

    Adding gfx110X nightlies only

--- build_tools/github_actions/amdgpu_family_matrix.py
+++ build_tools/github_actions/amdgpu_family_matrix.py
@@ -82,7 +82,7 @@ amdgpu_family_info_matrix_presubmit = {
             "fetch-gfx-targets": ["gfx1100"],
             "bypass_tests_for_releases": True,
             "build_variants": ["release"],
-            "sanity_check_only_for_family": True,
+            "nightly_check_only_for_family": True,
         },
     },
     "gfx1151": {
