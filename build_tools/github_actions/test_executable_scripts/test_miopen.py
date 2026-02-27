684c1066ec279113c76e7e4346c5e29d3b6948ab
aalabsi-amd <aalabsi@amd.com>
2026-02-27 13:43:23 -0500

    exclude long running tests

--- build_tools/github_actions/test_executable_scripts/test_miopen.py
+++ build_tools/github_actions/test_executable_scripts/test_miopen.py
@@ -117,6 +117,8 @@ negative_filter.append("*MIOpenTestConv*")
 
 # For sake of time saving on pre-commit step
 ####################################################
+negative_filter.append("Full/GPU_MIOpenDriverConv2dTransTest*")  # 4 min 45 sec
+negative_filter.append("Full/GPU_MIOpenDriverRegressionBigTensorTest_FP32") # 0 min 59 sec
 negative_filter.append("Full/GPU_Reduce_FP64*")  # 4 min 19 sec
 negative_filter.append("Full/GPU_BNOCLFWDTrainSerialRun3D_BFP16*")  # 3 min 37 sec
 negative_filter.append("Full/GPU_Lrn_FP32*")  # 2 min 50 sec
