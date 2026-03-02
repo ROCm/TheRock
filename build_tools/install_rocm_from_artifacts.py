74720a886467da2bb63a7e3f4d8712c0a8390073
chiranjeevi-amd <cpattigi@amd.com>
2026-03-02 11:51:17 +0530

    Revert "Add tests for rocprofiler-compute (#2300)"
    This reverts commit 5f60db78b53ca25f8bd6299d31f29a8b13791ed9.

--- build_tools/install_rocm_from_artifacts.py
+++ build_tools/install_rocm_from_artifacts.py
@@ -32,7 +32,6 @@ python build_tools/install_rocm_from_artifacts.py
     [--rocdecode | --no-rocdecode]
     [--rocjpeg | --no-rocjpeg]
     [--rocprofiler-compute | --no-rocprofiler-compute]
-    [--rocprofiler-sdk | --no-rocprofiler-sdk ]
     [--rocprofiler-systems | --no-rocprofiler-systems]
     [--rocrtst | --no-rocrtst]
     [--rocwmma | --no-rocwmma]
@@ -353,7 +352,6 @@ def retrieve_artifacts_by_run_id(args):
             args.rocdecode,
             args.rocjpeg,
             args.rocprofiler_compute,
-            args.rocprofiler_sdk,
             args.rocprofiler_systems,
             args.rocrtst,
             args.rocwmma,
@@ -754,16 +752,16 @@ def main(argv):
     )
 
     artifacts_group.add_argument(
-        "--rocprofiler-sdk",
+        "--rocprofiler-systems",
         default=False,
-        help="Include 'rocprofiler-sdk' artifacts",
+        help="Include 'rocprofiler-systems' artifacts",
         action=argparse.BooleanOptionalAction,
     )
 
     artifacts_group.add_argument(
-        "--rocprofiler-systems",
+        "--rocprofiler-sdk",
         default=False,
-        help="Include 'rocprofiler-systems' artifacts",
+        help="Include 'rocprofiler-sdk' artifacts",
         action=argparse.BooleanOptionalAction,
     )
 
