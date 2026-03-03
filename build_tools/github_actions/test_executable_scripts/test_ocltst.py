3baa94fb4fba6229aec6883a6bbabc94aff5c471
agunashe <ajay.gunashekar@amd.com>
2026-03-02 19:39:10 -0600

    include lib path and ocltst path to LD_LIBRARY_PATH

--- build_tools/github_actions/test_executable_scripts/test_ocltst.py
+++ build_tools/github_actions/test_executable_scripts/test_ocltst.py
@@ -29,31 +29,32 @@ def setup_env(env):
     env["ROCM_PATH"] = str(ROCM_PATH)
     if platform.system() == "Linux":
         OCL_LIB_PATH = Path(THEROCK_BIN_DIR).parent / "lib"
-        logging.info(f"++ Setting LD_LIBRARY_PATH={OCL_LIB_PATH}")
         if "LD_LIBRARY_PATH" in env:
             env["LD_LIBRARY_PATH"] = f"{OCL_LIB_PATH}:{env['LD_LIBRARY_PATH']}"
         else:
             env["LD_LIBRARY_PATH"] = OCL_LIB_PATH
 
-
 def execute_tests(env):
     if platform.system() == "Linux":
         OCLTST_PATH = str(Path(THEROCK_BIN_DIR).parent / "share" / "opencl" / "ocltst")
         cmd = [
-            "./ocltst", "-J", "-m", "oclruntime.so", "-A", "oclruntime.exclude",
+            "./ocltst", "-J", "-m", "liboclruntime.so", "-A", "oclruntime.exclude",
         ]
+        env["LD_LIBRARY_PATH"] = f"{env['LD_LIBRARY_PATH']}:{OCLTST_PATH}"
+        logging.info(f"++ Setting LD_LIBRARY_PATH={env['LD_LIBRARY_PATH']}")
+        shell_var = False
     elif platform.system() == "Windows":
         OCLTST_PATH = str(Path(THEROCK_BIN_DIR).parent / "tests" / "ocltst")
         cmd = [
             "ocltst.exe", "-J", "-m", "oclruntime.dll", "-A", "oclruntime.exclude",
         ]
+        shell_var = True
     else:
         logging.info(f"++ Error: unsupported system: {platform.system()}")
         sys.exit(1)
 
     logging.info(f"++ Exec [{OCLTST_PATH}]$ {shlex.join(cmd)}")
-    subprocess.run(cmd, cwd=OCLTST_PATH, check=True, env=env, shell=True)
-
+    subprocess.run(cmd, cwd=OCLTST_PATH, check=True, env=env, shell=shell_var)
 
 if __name__ == "__main__":
     setup_env(env)
