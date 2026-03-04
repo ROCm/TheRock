472a3651610a9b0bf771cd46a53821ae887822ab
agunashe <ajay.gunashekar@amd.com>
2026-03-04 14:56:52 -0600

    add opencl/lib, llvm/lib, rocm_sysdeps/lib to LD_LIBRARY_PATH

--- build_tools/github_actions/test_executable_scripts/test_ocltst.py
+++ build_tools/github_actions/test_executable_scripts/test_ocltst.py
@@ -28,12 +28,15 @@ def setup_env(env):
     ROCM_PATH = Path(THEROCK_BIN_DIR).resolve().parent
     env["ROCM_PATH"] = str(ROCM_PATH)
     if platform.system() == "Linux":
-        OCL_LIB_PATH = Path(THEROCK_BIN_DIR).parent / "lib"
-        logging.info(f"++ contents of OCL_LIB_PATH={os.listdir(OCL_LIB_PATH)}")
-        if "LD_LIBRARY_PATH" in env:
-            env["LD_LIBRARY_PATH"] = f"{OCL_LIB_PATH}:{env['LD_LIBRARY_PATH']}"
-        else:
-            env["LD_LIBRARY_PATH"] = OCL_LIB_PATH
+        ROCK_LIB_PATH = Path(THEROCK_BIN_DIR).parent / "lib"
+        OCL_LIB = Path(ROCK_LIB_PATH) / "opencl" / "lib"
+        LLVM_LIB = Path(ROCK_LIB_PATH) / "llvm" / "lib"
+        ROCM_SYSDEPS_LIB = Path(ROCK_LIB_PATH) / "rocm_sysdeps" / "lib"
+        logging.info(f"++ contents of ROCK_LIB_PATH={os.listdir(ROCK_LIB_PATH)}")
+        LD_LIBRARY_PATH = os.getenv("LD_LIBRARY_PATH")
+        if LD_LIBRARY_PATH is not None:
+            LD_LIBRARY_PATH = Path(LD_LIBRARY_PATH)
+        env["LD_LIBRARY_PATH"] = f"{ROCK_LIB_PATH}:{OCL_LIB}:{LLVM_LIB}:{ROCM_SYSDEPS_LIB}:{LD_LIBRARY_PATH}"
 
 def execute_tests(env):
     if platform.system() == "Linux":
@@ -42,7 +45,7 @@ def execute_tests(env):
             "./ocltst", "-J", "-m", "liboclruntime.so", "-A", "oclruntime.exclude",
         ]
         logging.info(f"++ contents of OCLTST_PATH={os.listdir(OCLTST_PATH)}")
-        env["LD_LIBRARY_PATH"] = f"{env['LD_LIBRARY_PATH']}:{OCLTST_PATH}"
+        env["LD_LIBRARY_PATH"] = f"{OCLTST_PATH}:{env['LD_LIBRARY_PATH']}"
         logging.info(f"++ Setting LD_LIBRARY_PATH={env['LD_LIBRARY_PATH']}")
         shell_var = False
     elif platform.system() == "Windows":
