32ffdc567b931a13a7e0cf8833a1a59ad3b4f054
Jessey Harrymanoharan <jharryma@amd.com>
2026-03-09 18:25:26 -0400

    Test test-gfx110X-gpu-rocm machines

--- build_tools/github_actions/amdgpu_family_matrix.py
+++ build_tools/github_actions/amdgpu_family_matrix.py
@@ -94,7 +94,7 @@ amdgpu_family_info_matrix_presubmit = {
             "sanity_check_only_for_family": True,
         },
         "windows": {
-            "test-runs-on": "windows-gfx110X-gpu-rocm",
+            "test-runs-on": "test-gfx110X-gpu-rocm",
             "family": "gfx110X-all",
             "fetch-gfx-targets": ["gfx1100"],
             "bypass_tests_for_releases": True,
