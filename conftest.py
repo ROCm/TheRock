8ccca35b942ada777dae80f52db8c3241fa9abc7
HRISHIKESH THULA <you@example.com>
2026-03-13 18:28:44 +0530

    Fix formatting of markers in conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 def pytest_configure(config):
     config.addinivalue_line(
-        "markers", 
+        "markers",
         "amdsmi_tests_default_unblocking_for_sanity: marks tests as default-unblocking for amdsmi sanity (amdsmi_tests_default_unblocking_for_sanity)",
     )
     config.addinivalue_line(
