9633d36c1b9cb4d17814d3d61822f3ed856fcc7c
geomin12 <geomin12@amd.com>
2026-04-01 11:06:44 -0700

    pr comments

--- build_tools/github_actions/configure_multi_arch_ci.py
+++ build_tools/github_actions/configure_multi_arch_ci.py
@@ -877,23 +877,11 @@ def write_outputs(
     linux = outputs.builds.linux
     windows = outputs.builds.windows
     test_type = outputs.jobs.test_rocm.test_type if outputs.is_ci_enabled else ""
-    # In the case of skipped CI, downstream workflows expect the build variant label to be set
-    default_build_variant_label = {
-        "build_variant_label": ci_inputs.build_variant,
-    }
     output_vars = {
         # Workflow YAML references this as 'enable_build_jobs'
         "enable_build_jobs": json.dumps(outputs.is_ci_enabled),
-        "linux_build_config": (
-            json.dumps(linux.to_dict())
-            if linux
-            else json.dumps(default_build_variant_label)
-        ),
-        "windows_build_config": (
-            json.dumps(windows.to_dict())
-            if windows
-            else json.dumps(default_build_variant_label)
-        ),
+        "linux_build_config": (json.dumps(linux.to_dict()) if linux else ""),
+        "windows_build_config": (json.dumps(windows.to_dict()) if windows else ""),
         "test_type": test_type,
         "linux_test_labels": outputs.linux_test_labels,
         "windows_test_labels": outputs.windows_test_labels,
