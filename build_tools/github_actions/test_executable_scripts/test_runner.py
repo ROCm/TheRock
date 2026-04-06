a31e8ae0c0951e6dd5fb6517b5bf69f0295ccd9d
Dileep Ravindranathan <dileep@dell-rack-13.amd.com>
2026-04-06 13:30:48 -0500

    Add rocroller to test_runner

--- build_tools/github_actions/test_executable_scripts/test_runner.py
+++ build_tools/github_actions/test_executable_scripts/test_runner.py
@@ -46,6 +46,7 @@ COMPONENT_DIR_MAPPING = {
     "hipdnn": "hipdnn",
     "hipdnn-samples": "hipdnn_samples",
     "miopen_plugin": "miopen_legacy_plugin",
+    "rocroller": "rocroller",
     # Add more mappings as needed
 }
 
