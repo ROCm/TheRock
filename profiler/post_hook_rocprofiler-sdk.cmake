8a5b6293797393db4475b46eb1700e58be0aabbd
jbonnell-amd <jason.bonnell@amd.com>
2026-04-07 09:56:23 -0400

    Add rocprofv3-avail to post_hook_rocprofiler-sdk.cmake

--- profiler/post_hook_rocprofiler-sdk.cmake
+++ profiler/post_hook_rocprofiler-sdk.cmake
@@ -11,3 +11,6 @@ set_target_properties(rocprofiler-sdk-tool-kokkosp PROPERTIES THEROCK_INSTALL_RP
 set_target_properties(rocprofv3-list-avail PROPERTIES THEROCK_INSTALL_RPATH_ORIGIN
   lib/rocprofiler-sdk
 )
+set_target_properties(rocprofv3-avail PROPERTIES THEROCK_INSTALL_RPATH_ORIGIN
+  lib/rocprofiler-sdk
+)
