79bedc3c05efa67ad20882b7daa0206564ac92d9
Stella Laurenzo <stellaraccident@gmail.com>
2026-04-13 11:31:18 -0700

    [WIP] Enable ROCR and rocminfo building on Windows.
    Dependent on: https://github.com/ROCm/rocm-systems/pull/4898

--- core/pre_hook_ROCR-Runtime.cmake
+++ core/pre_hook_ROCR-Runtime.cmake
@@ -4,5 +4,7 @@
 # Prefetch LibElf to nullify the shady embedded find modules.
 # Maddeningly, the find module calls itself "LibElf" (camel case) but sets
 # "LIBELF_FOUND" (uppercase).
-find_package(LibElf CONFIG REQUIRED)
-set(LIBELF_FOUND ON)
+if(NOT WIN32)
+  find_package(LibElf CONFIG REQUIRED)
+  set(LIBELF_FOUND ON)
+endif()
