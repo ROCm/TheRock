2fd8389980396c2b4b00b2e3eee984dc4b79357a
Randy J. Spaulding <rspauldi@amd.com>
2026-04-16 19:24:07 +0000

    use strix-halo-8gb

--- build_tools/github_actions/amdgpu_family_matrix.py
+++ build_tools/github_actions/amdgpu_family_matrix.py
@@ -113,7 +113,7 @@ amdgpu_family_info_matrix_presubmit = {
             "sanity_check_only_for_family": True,
         },
         "windows": {
-            "test-runs-on": "windows-gfx1151-gpu-rocm",
+            "test-runs-on": "windows-strix-halo-gpu-rocm-8gb",
             # TODO(#2754): Add new benchmark-runs-on runner for benchmarks
             "benchmark-runs-on": "windows-gfx1151-gpu-rocm",
             "family": "gfx1151",
