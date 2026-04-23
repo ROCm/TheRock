f1f1f1e43b42ac0d02b1d4b01f9e2c0b08fe89c8
geomin12 <geomin12@amd.com>
2026-04-23 15:22:57 -0700

    Adding pre-commit

--- build_tools/github_actions/fetch_test_configurations.py
+++ build_tools/github_actions/fetch_test_configurations.py
@@ -27,7 +27,10 @@ sys.path.insert(0, str(Path(__file__).resolve().parents[2] / "tests"))
 from github_actions_api import *
 from extended_tests.benchmark.benchmark_test_matrix import benchmark_matrix
 from extended_tests.functional.functional_test_matrix import functional_matrix
-from amdgpu_family_matrix import get_all_families_for_trigger_types, select_weighted_label
+from amdgpu_family_matrix import (
+    get_all_families_for_trigger_types,
+    select_weighted_label,
+)
 
 logging.basicConfig(level=logging.INFO)
 
