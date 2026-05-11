5476d1cbd3f2c83371cab1c9e0b46204ef3e1094
Joseph Macaranas <joseph.macaranas@amd.com>
2026-05-11 18:23:14 -0400

    RFC0010: validate MIOpen installed runner

--- build_tools/github_actions/fetch_test_configurations.py
+++ build_tools/github_actions/fetch_test_configurations.py
@@ -323,7 +323,7 @@ test_matrix = {
         "job_name": "miopen",
         "fetch_artifact_args": "--blas --miopen --rand --tests",
         "timeout_minutes": 60,
-        "test_script": f"python {_get_script_path('test_runner.py')}",
+        "test_script": "python ./build/bin/run_miopen.py",
         "platform": ["linux", "windows"],
         "total_shards_dict": {
             "linux": 4,
