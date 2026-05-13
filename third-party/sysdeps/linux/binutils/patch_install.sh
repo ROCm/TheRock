908d239a9c74d0d588d34789f8f9e6e5df573cfc
Aleksandar Janicijevic <ajanicij@amd.com>
2026-05-13 12:24:23 -0400

    Fixes for building binutils

--- third-party/sysdeps/linux/binutils/patch_install.sh
+++ third-party/sysdeps/linux/binutils/patch_install.sh
@@ -9,7 +9,7 @@ PATCHELF="${PATCHELF:-patchelf}"
 THEROCK_SOURCE_DIR="${THEROCK_SOURCE_DIR:?THEROCK_SOURCE_DIR not defined}"
 Python3_EXECUTABLE="${Python3_EXECUTABLE:?Python3_EXECUTABLE not defined}"
 
-echo "binutils::patch_install.sh - prefix - $PREFIX"
+echo "Patching binutils install..."
 
 # Copy lib64 to lib if it exists.
 if [ -d "$PREFIX/lib64" ]; then
@@ -19,6 +19,13 @@ fi
 
 # We don't want library descriptors or binaries.
 rm -f $PREFIX/lib/*.la
+
+# Remove shared libraries that are not consumed downstream.
+# Only libiberty.a is needed by rocprofiler-systems / dyninst.
+rm -f "$PREFIX/lib"/libbfd*.so* \
+      "$PREFIX/lib"/libopcodes*.so* \
+      "$PREFIX/lib"/libctf*.so* \
+      "$PREFIX/lib"/libsframe*.so*
 
 if [ -d "$PREFIX/lib/bfd-plugins" ]; then
   rm -rf $PREFIX/lib/bfd-plugins
@@ -38,3 +45,5 @@ fi
 if [ -d "$PREFIX/x86_64-pc-linux-gnu" ]; then
   rm -rf $PREFIX/x86_64-pc-linux-gnu
 fi
+
+echo "Done patching binutils install."
