063e1e7c1ed1ea29b8d21b655b93dc256731f30e
geomin12 <geomin12@amd.com>
2026-05-29 09:04:45 -0700

    fixing tests

--- build_tools/github_actions/tests/configure_target_run_test.py
+++ build_tools/github_actions/tests/configure_target_run_test.py
@@ -23,7 +23,7 @@ class ConfigureTargetRunTest(unittest.TestCase):
         # gfx94X-dcgpu is the inner key, which we use for package names. When
         # run from a workflow, we expect to only work on the inner keys.
         runner_label = configure_target_run.get_runner_label("gfx94X-dcgpu", "linux")
-        self.assertEqual(runner_label, "linux-gfx942-1gpu-ossci-rocm")
+        self.assertEqual(runner_label, "linux-gfx942-1gpu-core42-ossci-rocm")
 
     def test_windows_gfx115x(self):
         runner_label = configure_target_run.get_runner_label("gfx1151", "windows")
