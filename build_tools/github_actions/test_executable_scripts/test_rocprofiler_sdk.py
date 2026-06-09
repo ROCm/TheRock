2843218dae5ba04c189ee31eb73978b562a0cbe9
Ethan Stewart <ethan.stewart@amd.com>
2026-06-09 08:47:01 -0500

    [rocprofiler-sdk] - Move host-triple-logic to ensure variables used are defined

--- build_tools/github_actions/test_executable_scripts/test_rocprofiler_sdk.py
+++ build_tools/github_actions/test_executable_scripts/test_rocprofiler_sdk.py
@@ -18,17 +18,17 @@ THEROCK_LIB_PATH = THEROCK_PATH / "lib"
 THEROCK_SYSDEPS_PATH = THEROCK_LIB_PATH / "rocm_sysdeps"
 THEROCK_SYSDEPS_LIB_PATH = THEROCK_SYSDEPS_PATH / "lib"
 
+# LLVM Paths
+THEROCK_LLVM_BIN_PATH = THEROCK_PATH / "llvm" / "bin"
+THEROCK_CLANG_PATH = THEROCK_LLVM_BIN_PATH / "amdclang"
+THEROCK_CLANG_PLUS_PATH = THEROCK_LLVM_BIN_PATH / "amdclang++"
+
 # Determine host triple
 host_triple = subprocess.check_output(
     [str(THEROCK_CLANG_PATH), "--print-target-triple"], text=True
 ).strip()
 
-THE_ROCK_LLVM_LIB_HOST_TRIPLE_PATH = THE_ROCK_LIB_PATH / host_triple
-
-# LLVM Paths
-THEROCK_LLVM_BIN_PATH = THEROCK_PATH / "llvm" / "bin"
-THEROCK_CLANG_PATH = THEROCK_LLVM_BIN_PATH / "amdclang"
-THEROCK_CLANG_PLUS_PATH = THEROCK_LLVM_BIN_PATH / "amdclang++"
+THE_ROCK_LLVM_LIB_HOST_TRIPLE_PATH = THEROCK_LIB_PATH / "llvm" / "lib" / host_triple
 
 # SDK Paths
 ROCPROFILER_SDK_PATH = THEROCK_PATH / "share" / "rocprofiler-sdk"
