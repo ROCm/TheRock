274f4915f9a3787fcea5b99bfd74d78deece26df
Georgy Krivoruchko <georgy.krivoruchko@amd.com>
2026-06-11 13:35:34 +0400

    Improved build scripts for MSI packages

--- build_tools/packaging/windows/build-all.bat
+++ build_tools/packaging/windows/build-all.bat
@@ -1,13 +1,25 @@
 @echo off
+setlocal enabledelayedexpansion
 echo Building all available MSI packages...
 if not "%~1" == "" (
-rem Changing directory
-    %~d1
-    cd "%~dp1"
+    pushd "%~1" || exit /b 1
+    echo Changed directory to %~1
 )
 for /f %%I IN ('dir /b "%~dp0*-build.bat"') do (
   call "%~dp0%%I" /no_pause
+  if !ERRORLEVEL! neq 0 (
+    rem popd
+    rem echo Restored directory
+    rem exit /b 1
+  )
+)
+if not "%~1" == "" (
+    popd
+    echo Restored directory
 )
 echo Finished
-pause
+if not "%~2" == "/no_pause" (
+    pause
+)
 @echo on
+exit /b 0
