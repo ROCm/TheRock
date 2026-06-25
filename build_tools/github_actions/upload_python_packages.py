dcf62ab61c3a1629d3b172e91fdd21c0f66ac161
erman-gurses <erman.gurses@amd.com>
2026-06-25 02:19:15 +0000

    Add package_index_url output for better ROCm package indexing

--- build_tools/github_actions/upload_python_packages.py
+++ build_tools/github_actions/upload_python_packages.py
@@ -282,7 +282,11 @@ def run(args: argparse.Namespace):
         log("Set github actions output")
         log("-------------------------")
         gha_set_output(
-            {"package_find_links_url": index_url, "kpack_split": kpack_split}
+            {
+                "package_find_links_url": index_url,
+                "package_index_url": index_url,
+                "kpack_split": kpack_split,
+            }
         )
 
         log("Write github actions build summary")
