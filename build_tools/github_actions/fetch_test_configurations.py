136b4f7827cdfc37b4c34a8cd7fc50e0133bb818
Andrey Kasaurov <andrey.kasaurov@amd.com>
2026-06-30 16:15:14 -0400

    Expand rocRAND simulator coverage with new smoke preset (3 -> 17 gtests)

--- build_tools/github_actions/fetch_test_configurations.py
+++ build_tools/github_actions/fetch_test_configurations.py
@@ -411,7 +411,7 @@ test_matrix = {
         "job_name": "rocrand-simulator",
         "fetch_artifact_args": "--rand --rocjitsu --tests",
         "timeout_minutes": 60,
-        "test_script": f"python {_get_script_path('simulator_runner.py')} --component rocrand --filter-preset basic",
+        "test_script": f"python {_get_script_path('simulator_runner.py')} --component rocrand --filter-preset smoke",
         "platform": ["linux"],
         "linux_cpu_runner": True,
         "total_shards_dict": {
