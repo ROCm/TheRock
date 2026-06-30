c68be42217e14e83eee633852adb1ad0715b6f05
Danylo Lytovchenko <danylo.lytovchenko@amd.com>
2026-06-30 07:28:15 -0400

    Don't mark ignored CLI options as hard errors for MiOpen host asan builds

--- ml-libs/pre_hook_MIOpen.cmake
+++ ml-libs/pre_hook_MIOpen.cmake
@@ -10,3 +10,11 @@
 include_directories(SYSTEM "${THEROCK_BINARY_DIR}/math-libs/BLAS/hipBLAS/stage/include")
 include_directories(SYSTEM "${THEROCK_BINARY_DIR}/math-libs/BLAS/hipBLAS-common/stage/include")
 include_directories(SYSTEM "${THEROCK_BINARY_DIR}/math-libs/rocRAND/stage/include")
+
+# In HOST_ASAN mode, -fsanitize=address is passed globally but is not supported
+# for GPU device targets without xnack+. Clang emits -Woption-ignored for this,
+# which MIOpen's -Werror promotes to a hard error. Suppress it here since the
+# behavior (host instrumented, device not) is intentional in HOST_ASAN mode.
+if(THEROCK_SANITIZER STREQUAL "HOST_ASAN")
+  add_compile_options(-Wno-option-ignored)
+endif()
