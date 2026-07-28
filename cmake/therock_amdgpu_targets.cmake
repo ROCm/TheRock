eb7cf2ead676bb5200b1ca4a2f7b7bebff28bb38
idubinov <igor.dubinov@amd.com>
2026-07-28 04:23:48 -0500

    reorganize exclude list

--- cmake/therock_amdgpu_targets.cmake
+++ cmake/therock_amdgpu_targets.cmake
@@ -258,18 +258,63 @@ therock_add_amdgpu_target(gfx1201 "AMD RX 9070 / XT" FAMILY dgpu-all gfx120X-all
 therock_add_amdgpu_target(gfx1250 "AMD Instinct MI450/MI450X/MI455X CDNA" FAMILY dcgpu-all gfx125X-all gfx125X-dcgpu)
 
 # amdgcnspirv architecture-independent portable SPIR-V target
+# Note: some projects with GPU-specific components do not yet support SPIR-V so they are disabled here.
 therock_add_amdgpu_target(amdgcnspirv "AMDGPU portable SPIR-V" FAMILY gpu-generic
   EXCLUDE_TARGET_PROJECTS
-    MIOpen    ROCR-Runtime    aqlprofile    composable_kernel    hip-clr    hip-tests
-    hipBLAS    hipBLAS-common    hipBLASLt    hipCUB   hipDNN    hipDNN_samples
-    hipFFT    hipInfo    hipRAND    hipSOLVER    hipSPARSE    hipSPARSELt    hipTensor
-    hipblasltprovider    hipdnn_integration_tests    hipfile    hipkernelprovider
-    libhipcxx    miopenprovider    mirage    mxDataGenerator    ocl-clr    rccl
-    rccl-tests    rdc    rocALUTION    rocBLAS    rocFFT    rocPRIM    rocPRIM_tests
-    rocRAND    rocRoller    rocSOLVER    rocSPARSE    rocThrust    rocWMMA    rocdecode
-    rocjitsu    rocjpeg    rocm-kpack    rocprofiler-compute    rocprofiler-sdk
-    rocprofiler-systems    rocprofiler-systems-examples    rocr-debug-agent-tests
-    rocrtst    rocshmem    roctracer
+    MIOpen # https://github.com/ROCm/TheRock/issues/6918
+    ROCR-Runtime # https://github.com/ROCm/TheRock/issues/6918
+    aqlprofile # https://github.com/ROCm/TheRock/issues/6918
+    composable_kernel # https://github.com/ROCm/TheRock/issues/6918
+    hip-clr # https://github.com/ROCm/TheRock/issues/6918
+    hip-tests # https://github.com/ROCm/TheRock/issues/6918
+    hipBLAS # https://github.com/ROCm/TheRock/issues/6918
+    hipBLAS-common # https://github.com/ROCm/TheRock/issues/6918
+    hipBLASLt # https://github.com/ROCm/TheRock/issues/6918
+    hipCUB # https://github.com/ROCm/TheRock/issues/6918
+    hipDNN # https://github.com/ROCm/TheRock/issues/6918
+    hipDNN_samples # https://github.com/ROCm/TheRock/issues/6918
+    hipFFT # https://github.com/ROCm/TheRock/issues/6918
+    hipInfo # https://github.com/ROCm/TheRock/issues/6918
+    hipRAND # https://github.com/ROCm/TheRock/issues/6918
+    hipSOLVER # https://github.com/ROCm/TheRock/issues/6918
+    hipSPARSE # https://github.com/ROCm/TheRock/issues/6918
+    hipSPARSELt # https://github.com/ROCm/TheRock/issues/6918
+    hipTensor # https://github.com/ROCm/TheRock/issues/6918
+    hipblasltprovider # https://github.com/ROCm/TheRock/issues/6918
+    hipdnn_integration_tests # https://github.com/ROCm/TheRock/issues/6918
+    hipfile # https://github.com/ROCm/TheRock/issues/6918
+    hipkernelprovider # https://github.com/ROCm/TheRock/issues/6918
+    libhipcxx # https://github.com/ROCm/TheRock/issues/6918
+    miopenprovider # https://github.com/ROCm/TheRock/issues/6918
+    mirage # https://github.com/ROCm/TheRock/issues/6918
+    mxDataGenerator # https://github.com/ROCm/TheRock/issues/6918
+    ocl-clr # https://github.com/ROCm/TheRock/issues/6918
+    rccl # https://github.com/ROCm/TheRock/issues/6918
+    rccl-tests # https://github.com/ROCm/TheRock/issues/6918
+    rdc # https://github.com/ROCm/TheRock/issues/6918
+    rocALUTION # https://github.com/ROCm/TheRock/issues/6918
+    rocBLAS # https://github.com/ROCm/TheRock/issues/6918
+    rocFFT # https://github.com/ROCm/TheRock/issues/6918
+    rocPRIM # https://github.com/ROCm/TheRock/issues/6918
+    rocPRIM_tests # https://github.com/ROCm/TheRock/issues/6918
+    rocRAND # https://github.com/ROCm/TheRock/issues/6918
+    rocRoller # https://github.com/ROCm/TheRock/issues/6918
+    rocSOLVER # https://github.com/ROCm/TheRock/issues/6918
+    rocSPARSE # https://github.com/ROCm/TheRock/issues/6918
+    rocThrust # https://github.com/ROCm/TheRock/issues/6918
+    rocWMMA # https://github.com/ROCm/TheRock/issues/6918
+    rocdecode # https://github.com/ROCm/TheRock/issues/6918
+    rocjitsu # https://github.com/ROCm/TheRock/issues/6918
+    rocjpeg # https://github.com/ROCm/TheRock/issues/6918
+    rocm-kpack # https://github.com/ROCm/TheRock/issues/6918
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/6918
+    rocprofiler-sdk # https://github.com/ROCm/TheRock/issues/6918
+    rocprofiler-systems # https://github.com/ROCm/TheRock/issues/6918
+    rocprofiler-systems-examples # https://github.com/ROCm/TheRock/issues/6918
+    rocr-debug-agent-tests # https://github.com/ROCm/TheRock/issues/6918
+    rocrtst # https://github.com/ROCm/TheRock/issues/6918
+    rocshmem # https://github.com/ROCm/TheRock/issues/6918
+    roctracer # https://github.com/ROCm/TheRock/issues/6918
 )
 
 # Optional extension targets (used for out of tree target development).
