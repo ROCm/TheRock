f9253c19ad0ecb7553ba41421207377257197b32
koushikbillakanti-amd <koushik.billakanti@amd.com>
2026-07-29 09:59:53 -0500

    test: #9227 gate ON + guard post_hook_rocm_smi_lib (skip rocm_smi64/oam when disabled) - probe Tensile/rochpl consumers

--- base/post_hook_rocm_smi_lib.cmake
+++ base/post_hook_rocm_smi_lib.cmake
@@ -8,7 +8,7 @@
 # Conversely, if we include directories that do not exist, CMake consumers
 # will error out due to the missing directories.
 # See the project declaration for where this flag is defined.
-if(THEROCK_HAS_BUNDLED_LIBDRM)
+if(THEROCK_HAS_BUNDLED_LIBDRM AND TARGET rocm_smi64 AND TARGET oam)
   target_include_directories(rocm_smi64 PUBLIC
     "$<INSTALL_INTERFACE:lib/rocm_sysdeps/include>"
   )
