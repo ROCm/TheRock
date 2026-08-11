62906057105df33cd8e7f613a01c9527bb3d2655
gabrpham <Gabriel.Pham@amd.com>
2026-08-11 15:49:30 -0500

    Update post_hook_rocm_smi_lib to prevent failure of base build
    ROCM_SMI_DISABLE gate caused rocm_smi64 and oam to not be defined
    which in turn caused post_hook_rocm_smi_lib.cmake to return error.
    This prevented base from building which cascades down into
    rocprofiler_sdk failing and finally causing the compiler-runtime
    stage to fail. Added TARGET guards in post_hook_rocm_smi_lib.cmake
    to skip including those targets and unblock everything downstream.

--- base/post_hook_rocm_smi_lib.cmake
+++ base/post_hook_rocm_smi_lib.cmake
@@ -9,10 +9,14 @@
 # will error out due to the missing directories.
 # See the project declaration for where this flag is defined.
 if(THEROCK_HAS_BUNDLED_LIBDRM)
-  target_include_directories(rocm_smi64 PUBLIC
-    "$<INSTALL_INTERFACE:lib/rocm_sysdeps/include>"
-  )
-  target_include_directories(oam PUBLIC
-    "$<INSTALL_INTERFACE:lib/rocm_sysdeps/include>"
-  )
+  if(TARGET rocm_smi64)
+    target_include_directories(rocm_smi64 PUBLIC
+      "$<INSTALL_INTERFACE:lib/rocm_sysdeps/include>"
+    )
+  endif()
+  if(TARGET oam)
+    target_include_directories(oam PUBLIC
+      "$<INSTALL_INTERFACE:lib/rocm_sysdeps/include>"
+    )
+  endif()
 endif()
