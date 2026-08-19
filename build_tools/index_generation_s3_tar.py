e99fea039494a960533f13dead6244a6aefdc94b
Aravind Ravikumar <arravikum@amd.com>
2026-08-19 15:07:18 +0000

    Updating the page display title to 'Tarball index'

--- build_tools/index_generation_s3_tar.py
+++ build_tools/index_generation_s3_tar.py
@@ -99,7 +99,7 @@ def generate_index_s3(
         raise FileNotFoundError(f"No .tar.gz files found in bucket {bucket_name}.")
 
     # Page title
-    page_title = "Tarballs"
+    page_title = "Tarball index"
 
     # Prepare filter options and files array for JS
     gpu_families = extract_gpu_details(files)
