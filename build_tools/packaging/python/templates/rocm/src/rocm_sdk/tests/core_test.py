6e01d16549c05183b204c703a6826f97634093f6
Venkateshwar Reddy Kandula <vkandula@amd.com>
2026-09-10 11:05:21 -0500

    Update core_test.py to include additional path check
    Added condition to check for 'share/rocprofiler-sdk/tests/duplicate-sdk/' in so_path.

--- build_tools/packaging/python/templates/rocm/src/rocm_sdk/tests/core_test.py
+++ build_tools/packaging/python/templates/rocm/src/rocm_sdk/tests/core_test.py
@@ -102,6 +102,7 @@ class ROCmCoreTest(unittest.TestCase):
             if (
                 "lib/rocprofiler-sdk/" in str(so_path)
                 or "libexec/rocprofiler-sdk/" in str(so_path)
+                or "share/rocprofiler-sdk/tests/duplicate-sdk/" in str(so_path)
                 or "libpyrocpd" in str(so_path)
                 or "libpyroctx" in str(so_path)
             ):
