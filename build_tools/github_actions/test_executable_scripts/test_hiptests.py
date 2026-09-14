247b139ccdd20e325f6fbec8187adf0b4e8f90a4
Fabio Mestre <fabio.mestre@amd.com>
2026-09-14 12:09:44 +0000

    Re-enable hipMemcpyBatchAsync Swap tests

--- build_tools/github_actions/test_executable_scripts/test_hiptests.py
+++ build_tools/github_actions/test_executable_scripts/test_hiptests.py
@@ -90,9 +90,6 @@ TEST_TO_IGNORE = {
     "gfx125X-dcgpu": {
         "linux": [
             "Unit_hipGraphAddMemcpyNode1D_Positive_Basic",
-            # ROCM-29275: SDMA COPY_SWAP hangs the GPU on gfx1250 (rocm-systems#9923).
-            "Unit_hipMemcpyBatchAsync_Swap",
-            "Unit_hipMemcpyBatchAsync_P2P_Swap",
             # TODO(#7499): Re-enable test once crash issue is resolved.
             "Unit_hipVoteSync_All",
         ]
