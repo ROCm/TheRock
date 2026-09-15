842cf1a3e0ba6c7cc8c47c721d78181006babc2d
Venkateshwar Reddy Kandula <vkandula@amd.com>
2026-09-15 12:47:31 -0500

    Increase timeout for rocprofiler-sdk tests to 20 minutes

--- build_tools/github_actions/fetch_test_configurations.py
+++ build_tools/github_actions/fetch_test_configurations.py
@@ -618,7 +618,7 @@ test_matrix = {
     "rocprofiler-sdk": {
         "job_name": "rocprofiler-sdk",
         "fetch_artifact_args": "--tests",
-        "timeout_minutes": 15,
+        "timeout_minutes": 20,
         "additional_requirements_files": [
             "share/rocprofiler-sdk/tests/requirements.txt",
         ],
